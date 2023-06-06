c4cfba60d530a47251f4d153ae1e6c55b3756538
hftamayo <hftamayo@gmail.com>
2023-06-06 12:53:24 -0600

    connecting to express

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import "dotenv/config";
-import "./database/connectdb.js";
 import express from "express";
+import dotenv from "dotenv";
+import "./database/connectdb.js";
 
 const app = express();
-app.use(express.json());
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("http://localhost:" + PORT));
+app.listen(PORT, () => console.log("Server running on Port:" + PORT));
