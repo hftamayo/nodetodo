dcaa1904a17e3b0700301184657bee96ac13439d
hftamayo <hftamayo@gmail.com>
2023-06-08 14:19:19 -0600

    importing cookie parser

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import dbConnection from "./database/dbmanager.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config();
 
 dbConnection();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server running on Port:" + PORT));
