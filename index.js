e914db1874cddfd43c81f0579ae46354a0b598fa
hftamayo <hftamayo@gmail.com>
2023-06-08 14:53:39 -0600

    adding routes to the main component

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import dotenv from "dotenv";
 import dbConnection from "./database/dbmanager.js";
 import cookieParser from "cookie-parser";
 
+import todosRoutes from "./routes/todo.js";
+import usersRoutes from "./routes/user.js";
+
 const app = express();
 dotenv.config();
 
@@ -10,6 +13,8 @@ dbConnection();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use("/api/todos", todosRoutes);
+app.use("/api/users", usersRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server running on Port:" + PORT));
