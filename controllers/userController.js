d7f4c0aedb3e17019b117b2a88ad8e5e0a92a41e
hftamayo <hftamayo@gmail.com>
2023-06-08 15:23:01 -0600

    define methods schema in userController

---
+++ controllers/userController.js
@@ -0,0 +1,12 @@
+import User from "../models/User";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import Todo from "../models/Todo";
+
+export const register = async (req, res) => {};
+export const login = async (req, res) => {};
+export const logout = async (req, res) => {};
+export const getMe = async (req, res) => {};
+export const updateDetails = async (req, res) => {};
+export const updatePassword = async (req, res) => {};
+export const deleteUser = async (req, res) => {};
