7228f9af997a198966be9be8b94633e1191a7e71
hftamayo <hftamayo@gmail.com>
2023-06-14 14:59:26 -0600

    login method finished

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,34 +7,61 @@ export const register = async (req, res) => {
   const { name, email, password, age } = req.body;
 
   try {
-    let user = await User.findOne({email});
-    if(user){
-        return res.status(400).json({msg: "User already exists"});
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ msg: "User already exists" });
     }
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
     user = new User({
-        name,
-        email,
-        password: hashedPassword,
-        age,
+      name,
+      email,
+      password: hashedPassword,
+      age,
     });
     await user.save();
 
     const payload = {
-        user: user._id,
+      user: user._id,
     };
 
-    const token = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: 360000});
-    res.cookie("token", token, {httpOnly: true, expiresIn: 360000});
-    const {password: pass, ...rest} = user._doc;
-    res.status(201).json({msg: "User created successfully", user: rest});
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: 360000,
+    });
+    res.cookie("token", token, { httpOnly: true, expiresIn: 360000 });
+    const { password: pass, ...rest } = user._doc;
+    res.status(201).json({ msg: "User created successfully", user: rest });
   } catch (error) {
     console.log(error.message);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
-export const login = async (req, res) => {};
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ msg: "User not found" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch){
+        return res.status(400).json({msg: "Invalid Credentials"});
+    }
+    const payload = {
+        user: user._id,
+      };
+  
+      const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: 360000,
+      });
+      res.cookie("token", token, { httpOnly: true, expiresIn: 360000 });
+      const { password: pass, ...rest } = user._doc;
+      res.status(200).json({ msg: "User logged in successfully", user: rest });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ errors: "Internal Server Error" });
+  }
+};
 export const logout = async (req, res) => {};
 export const getMe = async (req, res) => {};
 export const updateDetails = async (req, res) => {};
