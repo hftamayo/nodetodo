98c8d80a63a8397b8eae67a5abfb21708a16fad2
hftamayo <hftamayo@gmail.com>
2023-06-22 11:39:27 -0600

    validation rules for each event part2

--- middleware/validator.js
+++ middleware/validator.js
@@ -18,7 +18,33 @@ export const loginRules = [
 ];
 
 export const updateDetailsRules = [
-    check("name", "Name is Required").notEmpty().trim().escape(),
-    check("email", "Please give a valid email").isEmail().normalizeEmail(),
-    check("age", "Age is required").notEmpty().trim().escape().isNumeric(),    
-]
+  check("name", "Name is Required").notEmpty().trim().escape(),
+  check("email", "Please give a valid email").isEmail().normalizeEmail(),
+  check("age", "Age is required").notEmpty().trim().escape().isNumeric(),
+];
+
+export const updatePasswordRules = [
+  check(
+    "password",
+    "Password should be at least 6 or more characters"
+  ).isLength({ min: 6 }),
+  check(
+    "newPassword",
+    "Password should be at least 6 or more characters"
+  ).isLength({ min: 6 }),
+];
+
+export const createTodoRules = [
+  check("title", "Title is Required").notEmpty().trim().escape(),
+  check("description", "Description is Required").notEmpty().trim().escape(),
+];
+
+export const updateTodoRules = [
+  check("title", "Title is Required").notEmpty().trim().escape(),
+  check("description", "Description is Required").notEmpty().trim().escape(),
+  check("completed", "Completed is Required")
+    .notEmpty()
+    .trim()
+    .escape()
+    .isBoolean(),
+];
