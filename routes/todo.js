9de81a04b76fd00c03f4e58a9bbdb58fda912cde
hftamayo <hftamayo@gmail.com>
2023-06-22 11:59:08 -0600

    adding validation to todo route and alpha1 ver

--- routes/todo.js
+++ routes/todo.js
@@ -7,12 +7,14 @@ import {
   updateTodo,
   deleteTodo,
 } from "../controllers/todoController.js";
+import { createTodoRules, updateTodoRules } from "../middleware/validator.js";
+import { validateResult } from "../middleware/validationResults.js";
 
 const router = express.Router();
 router.get("/:id", authorize, getTodo);
 router.get("/", authorize, getTodos);
-router.post("/create", authorize, createTodo);
-router.put("/update/:id", authorize, updateTodo);
+router.post("/create", authorize, createTodoRules, validateResult, createTodo);
+router.put("/update/:id", authorize, updateTodoRules, validateResult,  updateTodo);
 router.delete("/delete/:id", authorize, deleteTodo);
 
 export default router;
