ba3839d4dfd6c7ace0cad6c88005ecea8076923c
hftamayo <hftamayo@gmail.com>
2023-07-10 15:34:58 -0600

    updating import call

--- app.js
+++ app.js
@@ -4,21 +4,20 @@ import { init } from "./config/setup.js";
 
 import todosRoutes from "./api/routes/todo.js";
 import usersRoutes from "./api/routes/user.js";
-import dbConnection from "./database/dbmanager.js";
+
 
 const app = express();
 
-dbConnection();
+app.use(config.init);
+
+// dbConnection();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); //cuando false?
 app.use(cookieParser()); //que hace esta lib
-
-app.use(config.cors);
 
 app.use("/nodetodo/todos", todosRoutes);
 app.use("/nodetodo/users", usersRoutes);
 
 console.log("Application up and running");
 
-//module.exports = app;
 export default app;
