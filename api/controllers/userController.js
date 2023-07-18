ca66010f941c97c4269335fa0d4d8fd1a8d2fe71
hftamayo <hftamayo@gmail.com>
2023-07-18 14:42:02 -0600

    fixing some typos

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -3,91 +3,65 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import Todo from "../../models/Todo.js";
 
-const UserService = require("../../services/userService.js");
-
-// export const register = async (req, res) => {
-//   const { name, email, password, age } = req.body;
-
-//   try {
-//     let user = await User.findOne({ email });
-//     if (user) {
-//       return res.status(400).json({ msg: "User already exists" });
-//     }
-//     const salt = await bcrypt.genSalt(10);
-//     const hashedPassword = await bcrypt.hash(password, salt);
-//     user = new User({
-//       name,
-//       email,
-//       password: hashedPassword,
-//       age,
-//     });
-//     await user.save();
-
-//     const payload = {
-//       user: user._id,
-//     };
-
-//     const token = jwt.sign(payload, process.env.JWT_SECRET, {
-//       expiresIn: 360000, //expiresIn: "5h",
-//     });
-//     res.cookie("token", token, { httpOnly: true, expiresIn: 360000 });
-//     const { password: pass, ...rest } = user._doc;
-//     res.status(201).json({ msg: "User created successfully", user: rest });
-//   } catch (error) {
-//     console.log(error.message);
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// };
-
 export const register = async (req, res) => {
-  const { type, message } = await UserService.signUpUser(req.body);
+  const { name, email, password, age } = req.body;
 
-  if (type === 200) {
-    res.cookie("token", message, { httpOnly: true, expiresIn: "5h" });
-    res
-      .status(type)
-      .json({ title: "User created Successfully ", user: res });
+  try {
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ msg: "User already exists" });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    user = new User({
+      name,
+      email,
+      password: hashedPassword,
+      age,
+    });
+    await user.save();
+
+    const payload = {
+      user: user._id,
+    };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: 360000, //expiresIn: "5h",
+    });
+    res.cookie("token", token, { httpOnly: true, expiresIn: 360000 });
+    const { password: pass, ...rest } = user._doc;
+    res.status(201).json({ msg: "User created successfully", user: rest });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: "Internal Server Error" });
   }
-  res.status(type).json({ msg: message });
 };
 
-// export const login = async (req, res) => {
-//   const { email, password } = req.body;
-//   try {
-//     let user = await User.findOne({ email });
-//     if (!user) {
-//       return res.status(404).json({ msg: "User not found" });
-//     }
-//     const isMatch = await bcrypt.compare(password, user.password);
-//     if (!isMatch) {
-//       return res.status(400).json({ msg: "Invalid Credentials" });
-//     }
-//     const payload = {
-//       user: user._id,
-//     };
-
-//     const token = jwt.sign(payload, process.env.JWT_SECRET, {
-//       expiresIn: 360000,
-//     });
-//     res.cookie("token", token, { httpOnly: true, expiresIn: 360000 });
-//     const { password: pass, ...rest } = user._doc;
-//     res.status(200).json({ msg: "User logged in successfully", user: rest });
-//   } catch (error) {
-//     console.log(error.message);
-//     res.status(500).json({ errors: "Internal Server Error" });
-//   }
-// };
-
 export const login = async (req, res) => {
-  const { type, message } = await UserService.loginUser(req.body);
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ msg: "User not found" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Invalid Credentials" });
+    }
+    const payload = {
+      user: user._id,
+    };
 
-  if (type === 200) {
-    res.cookie("token", message, { httpOnly: true, expiresIn: "5h" });
-    res
-      .status(type)
-      .json({ title: "Logged in Successfully ", user: res });
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: 360000,
+    });
+    res.cookie("token", token, { httpOnly: true, expiresIn: 360000 });
+    const { password: pass, ...rest } = user._doc;
+    res.status(200).json({ msg: "User logged in successfully", user: rest });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ errors: "Internal Server Error" });
   }
-  res.status(type).json({ msg: message });
 };
 
 export const logout = async (req, res) => {
@@ -95,129 +69,87 @@ export const logout = async (req, res) => {
   res.status(200).json({ msg: "User logged out successfully" });
 };
 
-// export const getMe = async (req, res) => {
-//   try {
-//     const user = await User.findById(req.user);
-//     if (!user) {
-//       return res.status(404).json({ msg: "User Not Found" });
-//     }
-//     const { password: pass, ...rest } = user._doc;
-//     return res.status(200).json({ msg: "User found", user: rest });
-//   } catch (error) {
-//     console.error(error.message);
-//     res.status(500).json({ errors: "Internal Server Error" });
-//   }
-// };
-
 export const getMe = async (req, res) => {
-  const { type, message } = await UserService.listItemByID(req.user);
-  if (type === 200) {
-    res.status(type).json({ title: "User Found: ", msg: message });
+  try {
+    const user = await User.findById(req.user);
+    if (!user) {
+      return res.status(404).json({ msg: "User Not Found" });
+    }
+    const { password: pass, ...rest } = user._doc;
+    return res.status(200).json({ msg: "User found", user: rest });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ errors: "Internal Server Error" });
   }
-  res.status(type).json({ msg: message });
 };
 
-// export const updateDetails = async (req, res) => {
-//   const { name, email, age } = req.body;
-//   try {
-//     let user = await User.findById(req.user);
-//     if (!user) {
-//       return res.status(404).json({ msg: "User Not Found" });
-//     }
-//     let exists = await User.findOne({ email });
-//     if (exists && exists._id.toString() !== user._id.toString()) {
-//       return res.status(404).json({ msg: "Email already exists" });
-//     }
-//     user.name = name;
-//     user.email = email;
-//     user.age = age;
-
-//     await user.save();
-
-//     const { password: pass, ...rest } = user._doc;
-//     return res
-//       .status(200)
-//       .json({ msg: "User Updated Successfully", user: rest });
-//   } catch (error) {
-//     console.error(error.message);
-//     res.status(500).json({ errors: "Internal Server Error" });
-//   }
-// };
 export const updateDetails = async (req, res) => {
-  const { type, message } = await UserService.updateUserByID(
-    req.user,
-    req.body
-  );
-  if (type === 200) {
-    res
-      .status(type)
-      .json({ title: "User Updated Successfully! ", msg: message });
+  const { name, email, age } = req.body;
+  try {
+    let user = await User.findById(req.user);
+    if (!user) {
+      return res.status(404).json({ msg: "User Not Found" });
+    }
+    let exists = await User.findOne({ email });
+    if (exists && exists._id.toString() !== user._id.toString()) {
+      return res.status(404).json({ msg: "Email already exists" });
+    }
+    user.name = name;
+    user.email = email;
+    user.age = age;
+
+    await user.save();
+
+    const { password: pass, ...rest } = user._doc;
+    return res
+      .status(200)
+      .json({ msg: "User Updated Successfully", user: rest });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ errors: "Internal Server Error" });
   }
-  res.status(type).json({ msg: message });
 };
 
-// export const updatePassword = async (req, res) => {
-//   const { password, newPassword } = req.body;
-//   try {
-//     let user = await User.findById(req.user);
-//     if (!user) {
-//       return res.status(404).json({ msg: "User Not Found" });
-//     }
-//     const isMatch = await bcrypt.compare(password, user.password);
-//     if (!isMatch) {
-//       return res.status(400).json({ msg: "Invalid Credentials" });
-//     }
-//     const salt = await bcrypt.genSalt(10);
-//     user.password = await bcrypt.hash(newPassword, salt);
-//     await user.save();
-
-//     const { password: pass, ...rest } = user._doc;
-//     return res
-//       .status(200)
-//       .json({ msg: "Password updated successfully", user: rest });
-//   } catch (error) {
-//     console.error(error.message);
-//     res.status(500).json({ errors: "Internal Server Error" });
-//   }
-// };
 export const updatePassword = async (req, res) => {
-  const { type, message } = await UserService.updateUserPassword(
-    req.user,
-    req.body
-  );
+  const { password, newPassword } = req.body;
+  try {
+    let user = await User.findById(req.user);
+    if (!user) {
+      return res.status(404).json({ msg: "User Not Found" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Invalid Credentials" });
+    }
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(newPassword, salt);
+    await user.save();
 
-  if (type === 200) {
-    res
-      .status(type)
-      .json({ title: "Password updated successfully ", msg: message });
+    const { password: pass, ...rest } = user._doc;
+    return res
+      .status(200)
+      .json({ msg: "Password updated successfully", user: rest });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ errors: "Internal Server Error" });
   }
-  res.status(type).json({ msg: message });
 };
-
-// export const deleteUser = async (req, res) => {
-//   try {
-//     const user = await User.findById(req.user);
-//     if (!user) {
-//       return res.status(404).json({ msg: "User Not Found" });
-//     }
-//     const todo = await Todo.find({ user: req.user });
-//     if (todo) {
-//       await Todo.deleteMany({ user: req.user });
-//     }
-//     res.clearCookie("token");
-//     await user.deleteOne();
-//     res.status(200).json({ msg: "User deleted successfully" });
-//   } catch (error) {
-//     console.error(error.message);
-//     res.status(500).json({ errors: "Internal Server Error" });
-//   }
-// };
 
 export const deleteUser = async (req, res) => {
-  const { type, message } = await UserService.deleteUserByID(req.user);
-
-  if (type === 200) {
+  try {
+    const user = await User.findById(req.user);
+    if (!user) {
+      return res.status(404).json({ msg: "User Not Found" });
+    }
+    const todo = await Todo.find({ user: req.user });
+    if (todo) {
+      await Todo.deleteMany({ user: req.user });
+    }
     res.clearCookie("token");
+    await user.deleteOne();
+    res.status(200).json({ msg: "User deleted successfully" });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ errors: "Internal Server Error" });
   }
-  res.status(type).json({ msg: message });
 };
