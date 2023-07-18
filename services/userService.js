f1ed7b9c203315f4e3cc6a2025fa25cacbff52b3
hftamayo <hftamayo@gmail.com>
2023-07-18 14:41:22 -0600

    typing some ideas

--- services/userService.js
+++ services/userService.js
@@ -9,6 +9,23 @@ middlewares:
 3. Saltear el password prviamente validado
 */
 
+/*
+bugs identificados en la v1 del controller:
+1. msg no se muestra si intento registrar un usuario que ya existe
+2. en login sin usuario no existe mando un 404, 
+si las creds estan malas es un 400, esto puede ser un agujero de seguridad
+3. si me logeo correctamente se imprime la metadata de la cuenta de usuario
+4. getMe siempre va a devolver un 200 pues toma en cuenta la sesion activa 
+(que sucede en multisesion?)
+5. idem para logout, ambos request estan basados en una hash valido
+6. cuando el password se actualiza se vuelve a imprimir la metadata del usuario, 
+seguidamente debería hacerse un logout
+7. para actualizar y eliminar toma en cuenta la sesion activa, pero si por ejemplo,
+mando datos que ya existe lo deja pasar, asi mismo, si secuentro el token de la sesion
+puedo borrar el usuario
+
+*/
+
 export const signUpUser = async function (requestBody) {
   const { name, email, password, age } = requestBody.body;
   try {
