9e7ef5c43658b977ab2fd0c890a118565c300a0a
hftamayo <hftamayo@gmail.com>
2023-07-20 10:48:50 -0600

    cleaning unnec validation from controller methods

--- api/controllers/userController2.js
+++ api/controllers/userController2.js
@@ -12,7 +12,7 @@ export const register = async (req, res) => {
   if (type === 200) {
     res.cookie("token", message, { httpOnly: true, expiresIn: "5h" });
   }
-  res.status(type).json(message);
+  res.status(type).json({ msg: message });
 };
 
 export const login = async (req, res) => {
@@ -20,7 +20,6 @@ export const login = async (req, res) => {
 
   if (type === 200) {
     res.cookie("token", message, { httpOnly: true, expiresIn: "5h" });
-    res.status(type).json({ title: "Logged in Successfully ", user: res });
   }
   res.status(type).json({ msg: message });
 };
@@ -32,30 +31,16 @@ export const logout = async (req, res) => {
 
 export const getMe = async (req, res) => {
   const { type, message } = await listUserByID(req.user);
-  if (type === 200) {
-    res.status(type).json({ title: "User Found: ", msg: message });
-  }
   res.status(type).json({ msg: message });
 };
 
 export const updateDetails = async (req, res) => {
   const { type, message } = await updateUserByID(req.user, req.body);
-  if (type === 200) {
-    res
-      .status(type)
-      .json({ title: "User Updated Successfully! ", msg: message });
-  }
   res.status(type).json({ msg: message });
 };
 
 export const updatePassword = async (req, res) => {
   const { type, message } = await updateUserPassword(req.user, req.body);
-
-  if (type === 200) {
-    res
-      .status(type)
-      .json({ title: "Password updated successfully ", msg: message });
-  }
   res.status(type).json({ msg: message });
 };
 
