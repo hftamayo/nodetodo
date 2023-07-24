b1e0c631d9c3403efabd98b7a14d6b7e9647028c
hftamayo <hftamayo@gmail.com>
2023-07-24 12:48:57 -0600

    adding a descriptive message for unauthorized

--- api/middleware/authorize.js
+++ api/middleware/authorize.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 const authorize = async (req, res, next) => {
   const token = req.cookies.token;
   if (!token) {
-    return res.status(401).json({ msg: "Not authorized" });
+    return res.status(401).json({ msg: "Not authorized, please login first" });
   }
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
