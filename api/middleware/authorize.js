b0797da239d76e12a154027923abc7ef465296d5
hftamayo <hftamayo@gmail.com>
2023-07-24 13:09:09 -0600

    updating enviro vars

--- api/middleware/authorize.js
+++ api/middleware/authorize.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import { masterKey } from "../../config/envvars";
+import { masterKey } from "../../config/envvars.js";
 
 const authorize = async (req, res, next) => {
   const token = req.cookies.token;
