8808c02ddfbc7320d49be826738c6543577e6dcd
hftamayo <hftamayo@gmail.com>
2023-09-08 10:14:12 -0600

    debugging post method test part1

--- test/test.user.js
+++ test/test.user.js
@@ -3,39 +3,33 @@ process.env.NODE_ENV = "test";
 // const { token } = require("morgan");
 import mongoose from "mongoose";
 import User from "../models/User.js";
+
 import chai from "chai";
 import chaiHttp from "chai-http";
 import server from "../server.js";
 const expect = chai.expect;
+const should = chai.should;
+
 chai.use(chaiHttp);
 
-describe("Add an account for testing", () => {
-  before(function() {
-    proxy = process.env.CSJ_PROXY;
-  });
-
+describe("Adding a New User", () => {
+  before(function () {});
 
   it("POST /nodetodo/users/register", (done) => {
-    // let testUser = new User({
-    //   name: "tester",
-    //   email: "tester@tamayo.com",
-    //   password: "123456",
-    //   age: 40,
-    // });
+    let testUser = {
+      name: "tester7",
+      email: "tester7@tamayo.com",
+      password: "123456",
+      age: 40,
+    };
     chai
       .request(server)
       .post("/nodetodo/users/register")
-      .send({
-        name: "tester5",
-        email: "tester5@tamayo.com",
-        password: "123456",
-        age: 40,
-      })
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        //expect(res.body).to.have.property("name").eql("tester4");
-        //res.should.have.status(200);
-        // res.body.should.be.a("object");
+      .send(testUser)
+       .end((err, res) => {
+        //expect(res).to.have.status(200);
+        res.should.have.status(200);
+        res.body.should.be.a("object");
         //res.body.should.have.property("message").eql("User Added");
         // res.body.testUser.should.have.property("name").eql(testUser.name);
         // res.body.testUser.should.have.property("email").eql(testUser.email);
