d77bb7ba1f1701ad519779cce9fc47b5c385afca
hftamayo <hftamayo@gmail.com>
2023-09-27 15:43:06 -0600

    adding exec to get promises from mongoose

--- services/userService.js
+++ services/userService.js
@@ -7,7 +7,7 @@ import bcrypt from "bcrypt";
 export const signUpUser = async function (requestBody) {
   const { name, email, password, age } = requestBody;
   try {
-    let searchUser = await User.findOne({ email });
+    let searchUser = await User.findOne({ email }).exec();
     if (searchUser) {
       return { httpStatusCode: 400, message: "Email already exists" };
     }
@@ -30,7 +30,7 @@ export const signUpUser = async function (requestBody) {
 export const loginUser = async function (requestBody) {
   const { email, password } = requestBody;
   try {
-    let searchUser = await User.findOne({ email });
+    let searchUser = await User.findOne({ email }).exec();
     if (!searchUser) {
       return { httpStatusCode: 404, message: "User or Password does not match" };
     }
@@ -54,7 +54,7 @@ export const loginUser = async function (requestBody) {
 export const listUserByID = async function (requestUserId) {
   const userId = requestUserId;
   try {
-    let searchUser = await User.findById(userId);
+    let searchUser = await User.findById(userId).exec();
     if (!searchUser) {
       return { httpStatusCode: 404, message: "User Not Found" };
     }
@@ -70,11 +70,11 @@ export const updateUserByID = async function (requestUserId, requestBody) {
   const { name, email, age } = requestBody;
 
   try {
-    let searchUser = await User.findById(userId);
+    let searchUser = await User.findById(userId).exec();
     if (!searchUser) {
       return { httpStatusCode: 404, message: "User Not Found" };
     }
-    let checkIfExists = await User.findOne({ email });
+    let checkIfExists = await User.findOne({ email }).exec();
     if (checkIfExists && checkIfExists._id.toString() !== searchUser._id.toString()) {
       return { httpStatusCode: 400, message: "User Not Found" };
     }
@@ -95,7 +95,7 @@ export const updateUserPassword = async function (requestUserId, requestPword) {
   const userId = requestUserId;
   const { password, newPassword } = requestPword;
   try {
-    let searchUser = await User.findById(userId);
+    let searchUser = await User.findById(userId).exec();
     if (!searchUser) {
       return { httpStatusCode: 404, message: "User Not Found" };
     }
@@ -116,13 +116,13 @@ export const updateUserPassword = async function (requestUserId, requestPword) {
 export const deleteUserByID = async function (requestUserId) {
   const userId = requestUserId;
   try {
-    const searchUser = await User.findById(userId);
+    const searchUser = await User.findById(userId).exec();
     if (!searchUser) {
       return { httpStatusCode: 404, message: "User not found" };
     }
-    const todo = await Todo.find({ user: searchUser });
+    const todo = await Todo.find({ user: searchUser }).exec();
     if (todo) {
-      await Todo.deleteMany({ user: searchUser });
+      await Todo.deleteMany({ user: searchUser }).exec();
     }
     await searchUser.deleteOne();
     return { httpStatusCode: 200, message: "User deleted successfully" };
