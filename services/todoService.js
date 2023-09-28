717d6e9c95fcac714c0763f52cda31fb0d4beee3
hftamayo <hftamayo@gmail.com>
2023-09-28 15:01:59 -0600

    listTodoByID fversion 1

--- services/todoService.js
+++ services/todoService.js
@@ -1,31 +1,37 @@
 import Todo from "../../models/Todo.js";
 
-export const listActiveTodos = async function(requestUserId){
-    const userId = requestUserId;
-    try{
-        let activeTodos = await Todo.find({user: userId}).exec();
-        if(!activeTodos){
-            return { httpStatusCode: 404, message: "No active tasks found for this user" };
-        }
-        return {httpStatusCode: 200, message: "Tasks found", todos: activeTodos};
-
-    }catch(error){
-        console.error("todoService, getTodos: "+error.message);
-        return { httpStatusCode: 500, message: "Internal Server Error"};
-    }
-}
-
-export const getTodo = async (req, res) => {
-  const { id } = req.params;
+export const listActiveTodos = async function (requestUserId) {
+  const userId = requestUserId;
   try {
-    const todo = await Todo.findById(id);
-    if (!todo) {
-      return res.status(404).json({ msg: "Todo Not Found" });
+    let activeTodos = await Todo.find({ user: userId }).exec();
+    if (!activeTodos) {
+      return {
+        httpStatusCode: 404,
+        message: "No active tasks found for this user",
+      };
     }
-    if (todo.user.toString() !== req.user) {
-      return res.status(401).json({ msg: "Not Authorized" });
+    return { httpStatusCode: 200, message: "Tasks found", todos: activeTodos };
+  } catch (error) {
+    console.error("todoService, getTodos: " + error.message);
+    return { httpStatusCode: 500, message: "Internal Server Error" };
+  }
+};
+
+export const listTodoByID = async function (requestTodoId, requestBody) {
+  const todoId = requestTodoId;
+  const { userId } = requestBody;
+  try {
+    const searchTodo = await Todo.findById(todoId).exec();
+    if (!searchTodo) {
+      return { httpStatusCode: 404, message: "Task Not Found" };
     }
-    res.status(200).json({ msg: "Todo found", todo });
+    if (searchTodo.user.toString() !== userId) {
+      return {
+        httpStatusCode: 400,
+        message: "There's a problem with your credentials",
+      };
+    }
+    return { httpStatusCode: 200, msg: "Todo found", searchTodo };
   } catch (error) {
     console.error(error.message);
     res.status(500).send({ errors: "Internal Server Error" });
