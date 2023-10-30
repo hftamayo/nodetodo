5d4b9d1a3cc7e8901f991ea26659b59ccf6ca498
hftamayo <hftamayo@gmail.com>
2023-10-30 11:34:39 -0600

    userService unit test part1

---
+++ test/services/userServiceTest.js
@@ -0,0 +1,40 @@
+import { expect } from "chai";
+import {
+  signUpUser,
+  loginUser,
+  listUserByID,
+  updateUserByID,
+  updateUserPassword,
+  deleteUserByID,
+} from "../../services/userService";
+
+describe("UserService Unit Tests", () => {
+  describe("signupUser()", () => {
+    it("should create a new user with valid data", async () => {
+      const requestBody = {
+        name: "Herbert Fernandez Tamayo",
+        email: "hftamayo@gmail.com",
+        password: "milucito",
+        age: 40,
+      };
+      const response = await signUpUser(requestBody);
+      expect(response.httpStatusCode).to.equal(200);
+      expect(response.message).to.equal("User created successfully");
+      expect(response.user).to.exist;
+    });
+
+    it("should return an error if the user's email is already in use", async () => {
+      const requestBody = {
+        name: "Herbert Fernandez Tamayo",
+        email: "hftamayo@gmail.com",
+        password: "milucito",
+        age: 40,
+      };
+      await signUpUser(requestBody);
+
+      const response = await signUpUser(requestBody);
+      expect(response.httpStatusCode).to.equal(400);
+      expect(response.message).to.equal("Email already exists");
+    });
+  });
+});
