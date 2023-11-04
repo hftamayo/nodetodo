c27a467945fefd7b1b0b4c12755beae7f6e9802c
hftamayo <hftamayo@gmail.com>
2023-11-04 16:23:11 -0600

    user mock component

---
+++ test/mocks/user.mock.js
@@ -0,0 +1,8 @@
+import User from "../../models/User";
+
+export const mockUser = new User({
+  name: "Herbert Fernandez Tamayo",
+  email: "hftamayo@gmail.com",
+  password: "password",
+  age: 30,
+});
