77dadfaaabd483716a71a89965d6f50c08043a93
hftamayo <hftamayo@gmail.com>
2023-11-13 10:07:19 -0600

    extending user mock class

--- test/mocks/user.mock.js
+++ test/mocks/user.mock.js
@@ -1,8 +1,32 @@
 import User from "../../models/User";
 
 export const mockUser = new User({
+  id: "1234567890",
   name: "Herbert Fernandez Tamayo",
   email: "hftamayo@gmail.com",
   password: "password",
   age: 30,
 });
+
+export const mockUserInvalid = {
+  id: "123123123AAA",
+  name: "Herbert Fernandez",
+  email: "hftamayo2@gmail.com",
+  password: "incorrect",
+  age: 50,
+};
+
+export const mockUserUpdate = {
+  id: "11111111111",
+  name: "Sebastian Fernandez",
+  email: "sebas@gmail.com",
+  emailTaken: "hftamayo@gmail.com",
+  oldPassword: "password",
+  newPassword: "milucito",
+  notMatchPassword: "password2",
+  age: 20,
+};
+
+export const mockUserDelete = {
+  id: "222222222",
+};
