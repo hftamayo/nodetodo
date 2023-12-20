f1620d12a64db00fcf5e8de223546036c010a6cc
hftamayo <hftamayo@gmail.com>
2023-12-20 15:44:53 -0600

    pre-alpha of login with valid creds

--- test/controllers/userControllerIntegration.test.js
+++ test/controllers/userControllerIntegration.test.js
@@ -31,19 +31,37 @@ describe("User Controller Integration Test", function () {
       expect(response.body.newUser).to.have.property("age", mockUser.age);
     });
 
-    it.only("should not register an existing user", async function () {
-        this.timeout(10000);
-        const response = await request(server)
-          .post("/nodetodo/users/register")
-          .send({
-            name: mockUser.name,
-            email: mockUser.email,
-            password: mockUser.password,
-            age: mockUser.age,
-          });
-        expect(response.status).to.equal(400);
-        expect(response.body.resultMessage).to.equal("Email already exists");
-      });    
+    it("should not register an existing user", async function () {
+      this.timeout(10000);
+      const response = await request(server)
+        .post("/nodetodo/users/register")
+        .send({
+          name: mockUser.name,
+          email: mockUser.email,
+          password: mockUser.password,
+          age: mockUser.age,
+        });
+      expect(response.status).to.equal(400);
+      expect(response.body.resultMessage).to.equal("Email already exists");
+    });
 
+    it.only("should login a user with valid credentials", async function () {
+      this.timeout(10000);
+      const response = await request(server)
+        .post("/nodetodo/users/login")
+        .send({
+          email: mockUser.email,
+          password: mockUser.password,
+        });
+      expect(response.status).to.equal(200);
+      expect(response.body.resultMessage).to.equal("User login successfully");
+      expect(response.body.loggedUser).to.be.an("object");
+      expect(response.body.loggedUser).to.have.property("name", mockUser.name);
+      expect(response.body.loggedUser).to.have.property(
+        "email",
+        mockUser.email
+      );
+      expect(response.body.loggedUser).to.have.property("age", mockUser.age);
+    });
   });
 });
