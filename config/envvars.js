b6d6ccf3a41825ee88eaf69f0ca0b82adcffb82a
hftamayo <hftamayo@gmail.com>
2023-12-20 15:15:42 -0600

    evaluating enviro for uri selection

--- config/envvars.js
+++ config/envvars.js
@@ -3,7 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const port = parseInt(process.env.PORT);
-export const backend = process.env.MONGODB_URI;
 export const masterKey = process.env.JWT_SECRET;
 export const refreshKey = process.env.JWT_FRESH;
 export const mode = process.env.EXEC_MODE;
+
+export const backend =
+  mode === "production"
+    ? process.env.MONGODB_URI_PROD
+    : process.env.MONGODB_URI_DEV;
