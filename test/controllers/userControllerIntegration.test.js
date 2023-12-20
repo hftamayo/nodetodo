fddcc63d8392d0acc2db877be50363a71ab6cd03
hftamayo <hftamayo@gmail.com>
2023-12-20 14:45:34 -0600

    pre-alpha ver of registering a new user

---
+++ test/controllers/userControllerIntegration.test.js
@@ -0,0 +1,34 @@
+import request from "supertest";
+import { expect } from "chai";
+import server from "../../server.js";
+//import app from "../../app";
+import {
+  mockUser,
+  mockUserInvalid,
+  mockUserUpdate,
+  mockUserDelete,
+} from "../mocks/user.mock.js";
+
+describe("User Controller Integration Test", function () {
+  this.timeout(10000);
+  describe("POST /nodetodo/users/register", function () {
+    this.timeout(10000);
+    it("should register a new user", async function () {
+      this.timeout(10000);
+      const response = await request(server)
+        .post("/nodetodo/users/register")
+        .send({
+          name: mockUser.name,
+          email: mockUser.email,
+          password: mockUser.password,
+          age: mockUser.age,
+        });
+      expect(response.status).to.equal(200);
+      expect(response.body.resultMessage).to.equal("User created successfully");
+      expect(response.body.newUser).to.be.an("object");
+      expect(response.body.newUser).to.have.property("name", mockUser.name);
+      expect(response.body.newUser).to.have.property("email", mockUser.email);
+      expect(response.body.newUser).to.have.property("age", mockUser.age);
+    });
+  });
+});
