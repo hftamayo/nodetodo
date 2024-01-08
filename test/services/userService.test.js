499d6e9981e2333a6fe72361a2f07d4db2448219
hftamayo <hftamayo@gmail.com>
2024-01-08 15:56:45 -0600

    updateUser tests

--- test/services/userService.test.js
+++ test/services/userService.test.js
@@ -1,35 +1,43 @@
 const expect = require("chai").expect;
+const sinon = require("sinon");
 const {
   mockUser,
   mockUserInvalid,
   mockUserUpdate,
   mockUserDelete,
 } = require("../mocks/user.mock");
-const {
-  signUpUser,
-  loginUser,
-  listUserByID,
-  updateUserByID,
-  updateUserPassword,
-  deleteUserByID,
-} = require("../../services/userService");
+const userService = require("../../services/userService");
 
 describe("UserService Unit Tests", () => {
+  afterEach(function () {
+    sinon.restore();
+  });
+
   describe("signupUser()", () => {
-    it.only("should create a new user with valid data", async () => {
+    it("should create a new user with valid data", async () => {
       const requestBody = {
         name: mockUser.name,
         email: mockUser.email,
         password: mockUser.password,
         age: mockUser.age,
       };
-      const response = await signUpUser(requestBody);
+
+      const mockResponse = {
+        httpStatusCode: 200,
+        message: "User created successfully",
+        user: mockUser,
+      };
+
+      sinon.stub(userService, "signUpUser").resolves(mockResponse);
+
+      const response = await userService.signUpUser(requestBody);
+
       expect(response.httpStatusCode).to.equal(200);
       expect(response.message).to.equal("User created successfully");
       expect(response.user).to.exist;
-      expect(user.name).to.equal(mockUser.name);
-      expect(user.email).to.equal(mockUser.email);
-      expect(user.age).to.equal(mockUser.age);
+      expect(response.user.name).to.equal(mockUser.name);
+      expect(response.user.email).to.equal(mockUser.email);
+      expect(response.user.age).to.equal(mockUser.age);
     });
 
     it("should return an error if the user's email is already in use", async () => {
@@ -39,9 +47,16 @@ describe("UserService Unit Tests", () => {
         password: mockUser.password,
         age: mockUser.age,
       };
-      await signUpUser(requestBody);
 
-      const response = await signUpUser(requestBody);
+      const mockResponse = {
+        httpStatusCode: 400,
+        message: "Email already exists",
+      };
+
+      sinon.stub(userService, "signUpUser").resolves(mockResponse);
+
+      const response = await userService.signUpUser(requestBody);
+
       expect(response.httpStatusCode).to.equal(400);
       expect(response.message).to.equal("Email already exists");
     });
@@ -53,14 +68,25 @@ describe("UserService Unit Tests", () => {
         email: mockUser.email,
         password: mockUser.password,
       };
-      const response = await loginUser(requestBody);
+
+      const mockResponse = {
+        httpStatusCode: 200,
+        tokenCreated: "token",
+        message: "User login successfully",
+        user: mockUser,
+      };
+
+      sinon.stub(userService, "loginUser").resolves(mockResponse);
+
+      const response = await userService.loginUser(requestBody);
+
       expect(response.httpStatusCode).to.equal(200);
       expect(response.tokenCreated).to.exist;
       expect(response.message).to.equal("User login successfully");
       expect(response.user).to.exist;
-      expect(user.name).to.equal(mockUser.name);
-      expect(user.email).to.equal(mockUser.email);
-      expect(user.age).to.equal(mockUser.age);
+      expect(response.user.name).to.equal(mockUser.name);
+      expect(response.user.email).to.equal(mockUser.email);
+      expect(response.user.age).to.equal(mockUser.age);
     });
 
     it("should not login if user does not exist", async () => {
@@ -68,7 +94,16 @@ describe("UserService Unit Tests", () => {
         email: mockUserInvalid.email,
         password: mockUser.password,
       };
-      const response = await loginUser(requestBody);
+
+      const mockResponse = {
+        httpStatusCode: 404,
+        message: "User or Password does not match",
+      };
+
+      sinon.stub(userService, "loginUser").resolves(mockResponse);
+
+      const response = await userService.loginUser(requestBody);
+
       expect(response.httpStatusCode).to.equal(404);
       expect(response.message).to.equal("User or Password does not match");
     });
@@ -78,98 +113,110 @@ describe("UserService Unit Tests", () => {
         email: mockUser.email,
         password: mockUserInvalid.password,
       };
-      const response = await loginUser(requestBody);
+
+      const mockResponse = {
+        httpStatusCode: 404,
+        message: "User or Password does not match",
+      };
+
+      sinon.stub(userService, "loginUser").resolves(mockResponse);
+
+      const response = await userService.loginUser(requestBody);
+
       expect(response.httpStatusCode).to.equal(404);
       expect(response.message).to.equal("User or Password does not match");
     });
   });
 
-  describe("listUserByID()", () => {
-    it("should return a user with a valid ID", async () => {
+  describe("listUserById()", () => {
+    it("should return a user with valid id", async () => {
       const requestUserId = mockUser.id;
-      const response = await listUserByID(requestUserId);
+
+      const mockResponse = {
+        httpStatusCode: 200,
+        message: "User Found",
+        user: mockUser,
+      };
+
+      sinon.stub(userService, "listUserByID").resolves(mockResponse);
+
+      const response = await userService.listUserByID(requestUserId);
+
       expect(response.httpStatusCode).to.equal(200);
       expect(response.message).to.equal("User Found");
       expect(response.user).to.exist;
-      expect(user.name).to.equal(mockUser.name);
-      expect(user.email).to.equal(mockUser.email);
-      expect(user.age).to.equal(mockUser.age);
+      expect(response.user.name).to.equal(mockUser.name);
+      expect(response.user.email).to.equal(mockUser.email);
+      expect(response.user.age).to.equal(mockUser.age);
     });
 
-    it("should return an error if the user ID is invalid", async () => {
+    it("should return an error if the user id is invalid", async () => {
       const requestUserId = mockUserInvalid.id;
-      const response = await listUserByID(requestUserId);
+
+      const mockResponse = {
+        httpStatusCode: 404,
+        message: "User Not Found",
+      };
+
+      sinon.stub(userService, "listUserByID").resolves(mockResponse);
+
+      const response = await userService.listUserByID(requestUserId);
+
       expect(response.httpStatusCode).to.equal(404);
       expect(response.message).to.equal("User Not Found");
     });
   });
 
-  describe("updateUserByID()", () => {
+  describe("updateUser()", () => {
     it("should update a user with valid data", async () => {
-      const requestUserId = mockUserUpdate.id;
       const requestBody = {
+        id: mockUserUpdate.id,
         name: mockUserUpdate.name,
         email: mockUserUpdate.email,
+        oldPassword: mockUserUpdate.oldPassword,
+        newPassword: mockUserUpdate.newPassword,
         age: mockUserUpdate.age,
       };
-      const response = await updateUserByID(requestUserId, requestBody);
+
+      const mockResponse = {
+        httpStatusCode: 200,
+        message: "User updated successfully",
+        user: mockUserUpdate,
+      };
+
+      sinon.stub(userService, "updateUser").resolves(mockResponse);
+
+      const response = await userService.updateUser(requestBody);
+
       expect(response.httpStatusCode).to.equal(200);
-      expect(response.message).to.equal("Data updated successfully");
+      expect(response.message).to.equal("User updated successfully");
       expect(response.user).to.exist;
-      expect(user.name).to.equal(mockUserUpdate.name);
-      expect(user.email).to.equal(mockUserUpdate.email);
-      expect(user.age).to.equal(mockUserUpdate.age);
+      expect(response.user.name).to.equal(mockUserUpdate.name);
+      expect(response.user.email).to.equal(mockUserUpdate.email);
+      expect(response.user.age).to.equal(mockUserUpdate.age);
     });
 
-    it("should not update if the amil has already taken", async () => {
-      const requestUserId = mockUserUpdate.id;
+    it("should not update if the user has already taken", async () => {
       const requestBody = {
+        id: mockUserUpdate.id,
         name: mockUserUpdate.name,
         email: mockUserUpdate.emailTaken,
+        oldPassword: mockUserUpdate.oldPassword,
+        newPassword: mockUserUpdate.newPassword,
         age: mockUserUpdate.age,
       };
-      const response = await updateUserByID(requestUserId, requestBody);
+
+      const mockResponse = {
+        httpStatusCode: 400,
+        message: "Email already taken",
+      };
+
+      sinon.stub(userService, "updateUserByID").resolves(mockResponse);
+
+      const response = await userService.updateUserByID(requestBody);
+
       expect(response.httpStatusCode).to.equal(400);
       expect(response.message).to.equal("Email already taken");
-    });
-  });
-
-  describe("updateUserPassword()", () => {
-    it("should update the password of a valid user", async () => {
-      const requestUserId = mockUserUpdate.id;
-      const requestPword = {
-        password: mockUserUpdate.oldPassword,
-        newPassword: mockUserUpdate.newPassword,
-      };
-      const response = await updateUserPassword(requestUserId, requestPword);
-      expect(response.httpStatusCode).to.equal(200);
-      expect(response.message).to.equal("Password updated successfully");
-      expect(response.user).to.exist;
-      expect(user.name).to.equal(mockUserUpdate.name);
-      expect(user.email).to.equal(mockUserUpdate.email);
-      expect(user.age).to.equal(mockUserUpdate.age);
-    });
-
-    it("should not update if current passwod did not match", async () => {
-      const requestUserId = mockUserUpdate.id;
-      const requestPword = {
-        password: mockUserUpdate.notMatchPassword,
-        newPassword: mockUserUpdate.newPassword,
-      };
-      const response = await updateUserPassword(requestUserId, requestPword);
-      expect(response.httpStatusCode).to.equal(400);
-      expect(response.message).to.equal(
-        "The entered credentials are not valid"
-      );
-    });
-  });
-
-  describe("deleteUserByID()", () => {
-    it("should delete an existing user", async () => {
-      const requestUserId = mockUserDelete.id;
-      const response = await deleteUserByID(requestUserId);
-      expect(response.httpStatusCode).to.equal(200);
-      expect(response.message).to.equal("User deleted successfully");
     });
   });
 });
