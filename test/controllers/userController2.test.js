5e0bd55e3e4c6ad64149590d6de1bcaef749b292
hftamayo <hftamayo@gmail.com>
2024-01-10 10:45:20 -0600

    working wth proxyquire to mock userService calls

--- test/controllers/userController2.test.js
+++ test/controllers/userController2.test.js
@@ -1,21 +1,44 @@
 const sinon = require("sinon");
-const { expect } = require("chai");
+const proxyquire = require("proxyquire");
 const {
   mockUser,
   mockUserInvalid,
   mockUserUpdate,
   mockUserDelete,
 } = require("../mocks/user.mock");
-const User = require("../../models/User");
 const userController = require("../../api/controllers/userController");
 const userService = require("../../services/userService");
 
-describe("userController", () => {
-  describe("register", () => {
-    let sandbox;
+describe("userController Unit Test", () => {
+  describe("register method", () => {
+    let req, res, json, sandbox, userServiceStub, userController;
 
     beforeEach(() => {
       sandbox = sinon.createSandbox();
+
+      req = {
+        body: {
+          name: mockUser.name,
+          email: mockUser.email,
+          password: mockUser.password,
+          age: mockUser.age,
+        },
+      };
+      res = {};
+      json = sandbox.spy();
+      res.status = sandbox.stub().returns({ json });
+
+      userServiceStub = {
+        signUpUser: sandbox.stub().resolves({
+          httpStatusCode: 200,
+          message: "User created successfully",
+          user: mockUser,
+        }),
+      };
+
+      userController = proxyquire("../../api/controllers/userController", {
+        "../../services/userService": userServiceStub,
+      });
     });
 
     afterEach(() => {
@@ -24,44 +47,14 @@ describe("userController", () => {
 
     it("should register a new user", async function () {
       this.timeout(40000);
-      const req = {
-        body: {
-          name: mockUser.name,
-          email: mockUser.email,
-          password: mockUser.password,
-          age: mockUser.age,
-        },
-      };
-      const res = {
-        status: sandbox.stub().returnsThis(),
-        json: sandbox.stub(),
-      };
-
-      const mockUserInstance = {
-        save: sandbox.stub().resolves(mockUser),
-      };
-
-      sandbox.stub(User, "findOne").returns({
-        exec: sandbox.stub().resolves(mockUser),
-      });
-
-      sandbox.stub(User, "create").resolves(mockUserInstance);
-
-      sandbox.stub(userService, "signUpUser").resolves({
-        httpStatusCode: 200,
-        message: "User created successfully",
-        user: mockUser,
-      });
-
       await userController.register(req, res);
 
-      console.log(res.status);
-      console.log(res.json);
-
+      sinon.assert.calledOnce(userService.signUpUser);
+      sinon.assert.calledWith(userService.signUpUser, req.body);
       sinon.assert.calledOnce(res.status);
       sinon.assert.calledWith(res.status, 200);
-      sinon.assert.calledOnce(res.json);
-      sinon.assert.calledWith(res.json, {
+      sinon.assert.calledOnce(json);
+      sinon.assert.calledWith(json, {
         resultMessage: "User created successfully",
         newUser: mockUser,
       });
