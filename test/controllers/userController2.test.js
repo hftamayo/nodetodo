57cb0eb70de204bd827e3b9b922ae1d62830c6a6
hftamayo <hftamayo@gmail.com>
2024-01-11 11:35:27 -0600

    pre-alpha of register test case

--- test/controllers/userController2.test.js
+++ test/controllers/userController2.test.js
@@ -13,7 +13,13 @@ describe("userController Unit Test", () => {
 
     beforeEach(() => {
       sandbox = sinon.createSandbox();
+    });
 
+    afterEach(() => {
+      sandbox.restore();
+    });
+
+    it("should register a new user", async () => {
       req = {
         body: {
           name: mockUser.name,
@@ -31,13 +37,7 @@ describe("userController Unit Test", () => {
         message: "User created successfully",
         user: mockUser,
       });
-    });
 
-    afterEach(() => {
-      sandbox.restore();
-    });
-
-    it("should register a new user", async () => {
       await userController.register(req, res, signUpUserStub);
 
       const { password, ...filteredMockUser } = mockUser._doc;
@@ -50,6 +50,36 @@ describe("userController Unit Test", () => {
       sinon.assert.calledWith(json, {
         resultMessage: "User created successfully",
         newUser: filteredMockUser,
+      });
+    });
+
+    it("should restrict to register an existing user", async () => {
+      req = {
+        body: {
+          name: mockUserInvalid.name,
+          email: mockUserInvalid.email,
+          password: mockUserInvalid.password,
+          age: mockUserInvalid.age,
+        },
+      };
+      res = {};
+      json = sandbox.spy();
+      res.status = sandbox.stub().returns({ json });
+
+      signUpUserStub = sandbox.stub().resolves({
+        httpStatusCode: 400,
+        message: "Email already exists",
+      });
+
+      await userController.register(req, res, signUpUserStub);
+
+      sinon.assert.calledOnce(signUpUserStub);
+      sinon.assert.calledWith(signUpUserStub, req.body);
+      sinon.assert.calledOnce(res.status);
+      sinon.assert.calledWith(res.status, 400);
+      sinon.assert.calledOnce(json);
+      sinon.assert.calledWith(json, {
+        resultMessage: "Email already exists",
       });
     });
   });
