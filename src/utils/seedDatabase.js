22388a459d9a0d87b0602b778f5c83295754617c
hftamayo <hftamayo@gmail.com>
2024-01-29 10:05:23 -0600

    fixing a typo

--- src/utils/seedDatabase.js
+++ src/utils/seedDatabase.js
@@ -1,17 +1,17 @@
-const { mod, dataseeddev, dataseedprod } = require("../config/envvars");
+const { mode, dataseeddev, dataseedprod } = require("../config/envvars");
 
 const seedUsers = require("./seedUsers");
 
 async function seedDatabase() {
   try {
-    if (mod === "developer" || dataseeddev === "true") {
+    if (mode === "developer" || dataseeddev === "true") {
       console.log("Seeding the database in development environment...");
       await seedUsers();
-    } else if (mod === "production" || dataseedprod === "true") {
+    } else if (mode === "production" || dataseedprod === "true") {
       console.log("Seeding the database in production environment...");
       await seedUsers();
     }
-    console.log(`data seeding completed on ${mod} environment`);
+    console.log(`data seeding completed on ${mode} environment`);
   } catch (error) {
     console.error(
       "error in seeding database, impossible to continue: ",
