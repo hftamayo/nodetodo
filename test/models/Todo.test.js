9887224f21f1c65d89162cb9d49b5b2822a1884a
hftamayo <hftamayo@gmail.com>
2024-02-01 09:56:34 -0600

    missing description and completed test cases

--- test/models/Todo.test.js
+++ test/models/Todo.test.js
@@ -4,69 +4,96 @@ const Todo = require("../../src/models/Todo");
 const { newTodo, existingTodo } = require("../mocks/todo.mock");
 
 describe("Todo Model", () => {
-    it("should create a new todo with valid data", async () => {
-        const saveStub = sinon.stub(Todo.prototype, "save");
+  it("should create a new todo with valid data", async () => {
+    const saveStub = sinon.stub(Todo.prototype, "save");
 
-        const mockTodo = newTodo();
+    const mockTodo = newTodo();
 
-        saveStub.resolves(mockTodo);
+    saveStub.resolves(mockTodo);
 
-        const todo = new Todo(mockTodo);
+    const todo = new Todo(mockTodo);
 
-        await todo.save();
-    
-        expect(todo).to.exist;
-        expect(todo.title).to.equal(mockTodo.title);
-        expect(todo.description).to.equal(mockTodo.description);
-        expect(todo.completed).to.equal(mockTodo.completed);
-        expect(todo.user.toString()).to.equal(mockTodo.user.toString());
-    
-        saveStub.restore();
+    await todo.save();
+
+    expect(todo).to.exist;
+    expect(todo.title).to.equal(mockTodo.title);
+    expect(todo.description).to.equal(mockTodo.description);
+    expect(todo.completed).to.equal(mockTodo.completed);
+    expect(todo.user.toString()).to.equal(mockTodo.user.toString());
+
+    saveStub.restore();
+  });
+
+  it("should throw an error if the todo's title is missing", async () => {
+    const todo = new Todo({
+      title: "",
+      description: existingTodo.description,
+      completed: existingTodo.completed,
+      user: existingTodo.user,
     });
-    
-    it("should throw an error if the todo's title is missing", async () => {
-        const todo = new Todo({
-        title: "",
-        description: existingTodo.description,
-        completed: existingTodo.completed,
-        user: existingTodo.user,
-        });
-    
-        const saveStub = sinon.stub(Todo.prototype, "save");
-    
-        saveStub.rejects(new Error("Todo validation failed: `title` is required"));
-    
-        try {
-        await todo.save();
-        } catch (error) {
-        expect(error).to.exist;
-        expect(error.message).to.equal(
-            "Todo validation failed: `title` is required"
-        );
-        }
-        saveStub.restore();
+
+    const saveStub = sinon.stub(Todo.prototype, "save");
+
+    saveStub.rejects(new Error("Todo validation failed: `title` is required"));
+
+    try {
+      await todo.save();
+    } catch (error) {
+      expect(error).to.exist;
+      expect(error.message).to.equal(
+        "Todo validation failed: `title` is required"
+      );
+    }
+    saveStub.restore();
+  });
+
+  it("should throw an error if the todo's description is missing", async () => {
+    const todo = new Todo({
+      title: existingTodo.title,
+      description: "",
+      completed: existingTodo.completed,
+      user: existingTodo.user,
     });
-    
-    it("should throw an error if the todo's completed status is missing", async () => {
-        const todo = new Todo({
-        title: mockTodo.title,
-        completed: "",
-        });
-    
-        const saveStub = sinon.stub(Todo.prototype, "save");
-    
-        saveStub.rejects(
-        new Error("Todo validation failed: `completed` is required")
-        );
-    
-        try {
-        await todo.save();
-        } catch (error) {
-        expect(error).to.exist;
-        expect(error.message).to.equal(
-            "Todo validation failed: `completed` is required"
-        );
-        }
-        saveStub.restore();
+
+    const saveStub = sinon.stub(Todo.prototype, "save");
+
+    saveStub.rejects(
+      new Error("Todo validation failed: `description` is required")
+    );
+
+    try {
+      await todo.save();
+    } catch (error) {
+      expect(error).to.exist;
+      expect(error.message).to.equal(
+        "Todo validation failed: `description` is required"
+      );
+    }
+    saveStub.restore();
+  });
+
+  it("should throw an error if the todo's completed status is missing", async () => {
+    const todo = new Todo({
+      title: existingTodo.title,
+      description: existingTodo.description,
+      completed: "",
+      user: existingTodo.user,
     });
+
+    const saveStub = sinon.stub(Todo.prototype, "save");
+
+    saveStub.rejects(
+      new Error("Todo validation failed: `completed` is required")
+    );
+
+    try {
+      await todo.save();
+    } catch (error) {
+      expect(error).to.exist;
+      expect(error.message).to.equal(
+        "Todo validation failed: `completed` is required"
+      );
+    }
+    saveStub.restore();
+  });
 });
