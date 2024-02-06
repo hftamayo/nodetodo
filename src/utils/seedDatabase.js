5aae02b6c450b7e1bf3dedc5b2c0a0eaac17063d
hftamayo <hftamayo@gmail.com>
2024-02-06 11:35:21 -0600

    cleaning up the seeding process

--- src/utils/seedDatabase.js
+++ src/utils/seedDatabase.js
@@ -1,15 +1,19 @@
 const { mode, dataseeddev, dataseedprod } = require("../config/envvars");
 
 const seedUsers = require("./seedUsers");
+const todoUsers = require("./seedTodos");
 
 async function seedDatabase() {
   try {
-    if (mode === "development" || dataseeddev === "true") {
-      console.log("Seeding the database in development environment...");
+    if (
+      mode === "development" ||
+      dataseeddev === "true" ||
+      mode === "production" ||
+      dataseedprod === "true"
+    ) {
+      console.log(`Seeding the database in ${mode} environment...`);
       await seedUsers();
-    } else if (mode === "production" || dataseedprod === "true") {
-      console.log("Seeding the database in production environment...");
-      await seedUsers();
+      await todoUsers();
     } else {
       console.log("No seeding required");
     }
