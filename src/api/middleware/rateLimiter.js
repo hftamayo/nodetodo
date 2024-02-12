1a441777959b96fe3286d74d3cba7ac117c15b27
hftamayo <hftamayo@gmail.com>
2024-02-12 10:51:47 -0600

    improving rate limiting administration

--- src/api/middleware/rateLimiter.js
+++ src/api/middleware/rateLimiter.js
@@ -1,19 +1,18 @@
-require("dotenv").config();
-
-const isDev = process.env.NODE_ENV === "development";
-
+const { mode } = require("../../config/envvars");
 const rateLimit = require("express-rate-limit");
 
 const signUpLimiter = rateLimit({
-  windowMs: isDev ? 24 * 60 * 60 * 1000 : 60 * 60 * 1000,
-  max: isDev ? 10000 : 3,
+  //windowMs : 24 hours : 60 minutes
+  windowMs : mode === "development" ? 24 * 60 * 60 * 1000 : 60 * 60 * 1000,
+  max:  mode === "development" ? 10000 : 5,
   message:
     "Too many accounts created from this IP, please try again after an hour",
 });
 
 const loginLimiter = rateLimit({
-  windowMs: isDev ? 24 * 60 * 60 * 1000 : 15 * 60 * 1000,
-  max: isDev ? 10000 : 5,
+  //windowMs : 24 hours : 15 minutes
+  windowMs: mode === "development" ? 24 * 60 * 60 * 1000 : 15 * 60 * 1000,
+  max: mode === "development" ? 10000 : 3,
   message:
     "Too many login attempts from this IP, please try again after 15 minutes",
 });
