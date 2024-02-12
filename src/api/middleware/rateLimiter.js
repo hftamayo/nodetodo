d2c5eccd01825adb1f53962628bf4c4972cb3de7
hftamayo <hftamayo@gmail.com>
2024-02-12 08:56:24 -0600

    adding parameters for dev environment

--- src/api/middleware/rateLimiter.js
+++ src/api/middleware/rateLimiter.js
@@ -1,17 +1,19 @@
+const isDev = process.env.NODE_ENV === "development";
+
 const rateLimit = require("express-rate-limit");
 
 const signUpLimiter = rateLimit({
-  windowMs: 60 * 60 * 1000,
-  max: 3,
+  windowMs: isDev ? 24 * 60 * 60 * 1000 : 60 * 60 * 1000,
+  max: isDev ? 10000 : 3,
   message:
     "Too many accounts created from this IP, please try again after an hour",
 });
 
 const loginLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 5,
-    message:
-      "Too many login attempts from this IP, please try again after 15 minutes",
+  windowMs: isDev ? 24 * 60 * 60 * 1000 : 15 * 60 * 1000,
+  max: isDev ? 10000 : 5,
+  message:
+    "Too many login attempts from this IP, please try again after 15 minutes",
 });
 
 module.exports = { signUpLimiter, loginLimiter };
