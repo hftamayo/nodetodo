2fd83a792c7d6dafc04a2dc9437cb17d627222c0
hftamayo <hftamayo@gmail.com>
2024-02-15 10:39:49 -0600

    removing unneeded blank space

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const cookieParser = require("cookie-parser");
 const { dbConnection, setCorsEnviro } = require("./config/setup");
 const { port } = require("./config/envvars");
 
-const  seedDatabase  = require("./utils/seedDatabase");
+const seedDatabase = require("./utils/seedDatabase");
 const todosRoutes = require("./api/routes/todo");
 const usersRoutes = require("./api/routes/user");
 const healthCheckRoutes = require("./api/routes/healthCheck");
