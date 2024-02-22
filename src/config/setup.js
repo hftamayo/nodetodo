c4427864571d59a91e32e3dfe88c73bebb4d254d
hftamayo <hftamayo@gmail.com>
2024-02-22 08:45:16 -0600

    changing how cors is set on the application

--- src/config/setup.js
+++ src/config/setup.js
@@ -1,4 +1,3 @@
-const cors = require("cors");
 const mongoose = require("mongoose");
 const { backend, whitelist_frontend } = require("./envvars");
 
@@ -19,36 +18,33 @@ const dbConnection = async () => {
   }
 };
 
-const setCorsEnviro = (app) => {
-  app.use(
-    cors({
-      origin: function (origin, callback) {
-        if (whitelist_frontend.indexOf(origin) !== -1 || !origin) {
-          console.log(`CORS requested from origin: ${origin} granted`);
-          callback(null, true);
-        } else {
-          callback(new Error(`CORS requested from origin: ${origin} denied`));
-        }
-      },
-      credentials: true,
-      methods: ["GET","HEAD","PUT","PATCH","POST","DELETE"],
-      allowedHeaders: [
-        "Access-Control-Allow-Headers",
-        "Access-Control-Allow-Origin",
-        "Access-Control-Allow-Methods",
-        "Access-Control-Allow-Credential",
-        "Origin",
-        "withCredentials",
-        "X-Requested-With",
-        "Content-Type",
-        "Accept",
-        "Authorization",
-        "X-HTTP-Method-Override",
-        "Set-Cookie",
-        "Cookie",
-        "Request",
-      ],
-    })
-  );
+const setCorsEnviro = {
+  origin: (origin, callback) => {
+    if (whitelist_frontend.indexOf(origin) !== -1 || !origin) {
+      console.log(`CORS requested from origin: ${origin} granted`);
+      callback(null, true);
+    } else {
+      callback(new Error(`CORS requested from origin: ${origin} denied`));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200,
+  methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+  allowedHeaders: [
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Origin",
+    "Access-Control-Allow-Methods",
+    "Access-Control-Allow-Credential",
+    "Origin",
+    "withCredentials",
+    "X-Requested-With",
+    "Content-Type",
+    "Accept",
+    "Authorization",
+    "X-HTTP-Method-Override",
+    "Set-Cookie",
+    "Cookie",
+    "Request",
+  ],
 };
 module.exports = { dbConnection, setCorsEnviro };
