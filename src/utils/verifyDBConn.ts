910733412e51f74ad2fa2f26568b49673ba34282
hftamayo <hftamayo@gmail.com>
2024-03-07 11:24:21 -0600

    translating verifyDB

--- src/utils/verifyDBConn.ts
+++ src/utils/verifyDBConn.ts
@@ -1,21 +1,21 @@
 import mongoose from "mongoose";
 
-try {
-  await mongoose.connect(process.env.MONGODB_URI);
-  mongoose.connection.on("open", function () {
-    mongoose.connection.db.collectionNames(function (error, names) {
-      if (error) {
-        throw new Error(error);
-      } else {
-        names.map(function (name) {
-          console.log("found collection %s", name);
-        });
-      }
+async function verifyDBConn() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI as string);
+    const collections = await mongoose.connection.db
+      .listCollections()
+      .toArray();
+
+    collections.map((collection) => {
+      console.log("found collection %s", collection.name);
     });
-  });
-  console.log("Connection established with Data Repository: Remote");
-} catch (error) {
-  console.log(
-    "Couldn't established connection with Data Repository, details: " + error
-  );
+    console.log("Connection established with Data Repository: Remote");
+  } catch (error) {
+    console.log(
+      "Couldn't established connection with Data Repository, details: " + error
+    );
+  }
 }
+
+export default verifyDBConn;
