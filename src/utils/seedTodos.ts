e318e812ec8f6844e351ecdba68d174e31d4112b
hftamayo <hftamayo@gmail.com>
2024-03-19 12:53:13 -0600

    translating seedTodos

--- src/utils/seedTodos.ts
+++ src/utils/seedTodos.ts
@@ -1,24 +1,36 @@
-const mongoose = require("mongoose");
-const Todo = require("../models/Todo");
+import mongoose from "mongoose";
+import Todo from "../models/Todo";
 
-const seedTodos = async function () {
-  const todos = [
-    {
-      _id: new mongoose.Types.ObjectId("5f7f8b1e9f3f9c1d6c1e4d1d"),
-      title: "Foreign language class",
-      description: "To learn a new language",
-      completed: false,
-      user: "5f7f8b1e9f3f9c1d6c1e4d1e",
-    },
-  ];
+interface Todo {
+  _id: mongoose.Types.ObjectId;
+  title: string;
+  description: string;
+  completed: boolean;
+  user: mongoose.Types.ObjectId;
+};
 
+const todos: Todo[] = [
+  {
+    _id: new mongoose.Types.ObjectId("5f7f8b1e9f3f9c1d6c1e4d1d"),
+    title: "Foreign language class",
+    description: "To learn a new language",
+    completed: false,
+    user: new mongoose.Types.ObjectId("5f7f8b1e9f3f9c1d6c1e4d1e"),
+  },
+];
+
+async function seedTodos() {
   try {
     await Todo.deleteMany({});
     const todosCreated = await Todo.create(todos);
     console.log("Todos created: ", todosCreated);
-  } catch (error) {
-    console.error("seedTodos: ", error.message);
+  } catch (error: unknown) {
+    if (error instanceof Error){
+      console.error("seedTodos: ", error.message);
+    } else {
+      console.error("seedTodos: ", error);
+    }
   }
-};
+}
 
-module.exports = seedTodos;
+export default seedTodos;
