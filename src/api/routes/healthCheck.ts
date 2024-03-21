8faeb4c33204d635e2aaf411db5555e67750b945
hftamayo <hftamayo@gmail.com>
2024-03-21 09:35:12 -0600

    translating healthCheck to ts

--- src/api/routes/healthCheck.ts
+++ src/api/routes/healthCheck.ts
@@ -1,6 +1,7 @@
-const express = require("express");
+import express from "express";
+
 const router = express.Router();
-const { dbConnection } = require("../../config/setup");
+import { dbConnection } from "../../config/setup";
 
 router.get("/app", (req, res) => {
   res.status(200).json({
@@ -17,13 +18,13 @@ router.get("/db", async (req, res) => {
         "HealthCheck: The connection to the data layer is up and running",
       timestamp: new Date().toISOString(),
     });
-  } catch (error) {
+  } catch (error: unknown) {
     res.status(500).json({
       message: "HealthCheck: The connection to the data layer is down",
       timestamp: new Date().toISOString(),
-      error: error.message,
+      error: error instanceof Error ? error.message : error,
     });
   }
 });
 
-module.exports = router;
+export default router;
