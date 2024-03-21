f495dc0a00605eebab9493e1318cd25c3fd8a1ec
hftamayo <hftamayo@gmail.com>
2024-03-21 09:48:54 -0600

    translating validationResults middleware

--- src/api/middleware/validationResults.ts
+++ src/api/middleware/validationResults.ts
@@ -1,11 +1,12 @@
-const  {validationResult}  = require("express-validator");
+import { Request, Response, NextFunction } from "express";
+import { validationResult, Result, ValidationError } from "express-validator";
 
-const validateResult = (req, res, next) => {
-  const errors = validationResult(req);
+const validateResult = (req: Request, res: Response, next: NextFunction) => {
+  const errors: Result<ValidationError> = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ msg: errors.array()[0].msg });
   }
   next();
 };
 
-module.exports.validateResult = validateResult
+export default validateResult;
