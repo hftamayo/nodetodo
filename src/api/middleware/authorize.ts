9433550da4be321042c438ce69cae1f966e1d49e
Herbert Tamayo <hftamayo@gmail.com>
2024-04-09 12:34:42 -0600

    moving user interface to types subdir

--- src/api/middleware/authorize.ts
+++ src/api/middleware/authorize.ts
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
 import { masterKey } from "../../config/envvars";
-import { User } from "./user.interface";
+import { User } from "./types/user.interface";
 import { JwtPayloadWithUser } from "./types/user-jwt.interface";
 
 interface RequestWithUser extends Request {
