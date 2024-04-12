7deddbc847a7faa994fe5610a05ac6d4585c3317
Herbert Tamayo <hftamayo@gmail.com>
2024-04-12 11:11:07 -0600

    adding RequestWithUserBody interface

--- src/types/user.interface.ts
+++ src/types/user.interface.ts
@@ -1,26 +1,29 @@
 export interface UserRequestBody {
-    id: string;
-    name: string;
-    email: string;
-    age: number;
-    password: string;
-    newPassword: string;
+  id: string;
+  name: string;
+  email: string;
+  age: number;
+  password: string;
+  newPassword: string;
 }
 
 export interface UserId {
-    id: string;
+  id: string;
 }
 
 export interface RequestWithUserId extends Request {
-    user: UserId;
+  user: UserId;
+}
+
+export interface RequestWithUser extends Request {
+  user: UserRequestBody;
 }
 
 export interface UserControllerResult {
-    httpStatusCode: number;
-    tokenCreated: string;
-    message: string;
-    user: UserRequestBody;
+  httpStatusCode: number;
+  tokenCreated: string;
+  message: string;
+  user: UserRequestBody;
 }
 
 export type PartialUserRequestBody = Partial<UserRequestBody>;
-
