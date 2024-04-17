5deffd9c7800ecfa51d4d6689fac0817ca1f2a8c
Herbert Tamayo <hftamayo@gmail.com>
2024-04-17 10:49:02 -0600

    defining an updateResult for output workflow

--- src/types/user.interface.ts
+++ src/types/user.interface.ts
@@ -28,11 +28,22 @@ export interface RequestWithUserBody extends Request {
   user: UserRequestBody;
 }
 
+export interface RequestWithUserUpdateBody extends Request {
+  userId: UserId;
+  user: UserRequestUpdateBody;
+}
+
 export interface UserControllerResult {
   httpStatusCode: number;
   tokenCreated: string;
   message: string;
   user: UserRequestBody;
+}
+
+export interface UserUpdateControllerResult {
+  httpStatusCode: number;
+  message: string;
+  updatedUser: UserRequestBody;
 }
 
 export type PartialUserRequestBody = Partial<UserRequestBody>;
