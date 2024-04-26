f95b796e83657162ff6934e5a401afe1236c5085
Herbert Tamayo <hftamayo@gmail.com>
2024-04-26 10:37:59 -0600

    adding a DeleteTodoRequest interface

--- src/types/todo.interface.ts
+++ src/types/todo.interface.ts
@@ -1,3 +1,5 @@
+import { UserIdRequest } from "./user.interface";
+
 export interface TodoRequest{
     id?: string;
     title: string;
@@ -14,6 +16,13 @@ export interface UpdateTodoRequest{
     owner: string;
     todoId: string;
     todo: Partial<TodoRequest>;
+}
+
+export interface DeleteTodoRequest extends Request {
+    user: UserIdRequest;
+    params: {
+        id: TodoIdRequest;
+    }
 }
 
 export interface TodoResult{
