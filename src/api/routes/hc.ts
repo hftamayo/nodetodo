29915e2ddb8ebd317cc6ce12ad72bbdb124ea546
Herbert Tamayo <hftamayo@gmail.com>
2024-04-29 09:24:04 -0600

    updating healthCheck route

--- src/api/routes/hc.ts
+++ src/api/routes/hc.ts
@@ -1,30 +1,9 @@
-import express, {Request, Response} from "express";
-import { dbConnection } from "../../config/setup";
+import express from "express";
+import hcController from "../controllers/hcController";
 
 const router = express.Router();
 
-router.get("/app", (req: Request, res: Response) => {
-  res.status(200).json({
-    message: "HealthCheck: Application is up and running",
-    timestamp: new Date().toISOString(),
-  });
-});
-
-router.get("/db", async (req: Request, res: Response) => {
-  try {
-    await dbConnection();
-    res.status(200).json({
-      message:
-        "HealthCheck: The connection to the data layer is up and running",
-      timestamp: new Date().toISOString(),
-    });
-  } catch (error: unknown) {
-    res.status(500).json({
-      message: "HealthCheck: The connection to the data layer is down",
-      timestamp: new Date().toISOString(),
-      error: error instanceof Error ? error.message : error,
-    });
-  }
-});
+router.get("/app", hcController.appHealthCheck);
+router.get("/db", hcController.dbHealthCheck);
 
 export default router;
