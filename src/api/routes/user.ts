94e7789310efee80b53796973f8659886a194d22
Herbert Tamayo <hftamayo@gmail.com>
2024-05-23 09:37:10 -0600

    updating dependency management

--- src/api/routes/user.ts
+++ src/api/routes/user.ts
@@ -1,81 +1,54 @@
 import express, { Request, Response } from "express";
 import userController from "../controllers/userController";
-import userService from "../../services/userService";
 import authorize from "../middleware/authorize";
 import validator from "../middleware/validator";
 import validateResult from "../middleware/validationResults";
 import rateLimiter from "../middleware/rateLimiter";
 import {
   UserRequest,
+  LoginRequest,
   UserIdRequest,
-  UserResult,
   UpdateUserRequest,
+  UserServices
 } from "../../types/user.interface";
+
 
 const router = express.Router();
 
-userController.setSignUpUser(userService.signUpUser);
-userController.setLoginUser(
-  userService.loginUser as (newLoginUser: UserRequest) => Promise<UserResult>
-);
-userController.setListUser(
-  userService.listUserByID as (
-    newListUser: UserIdRequest
-  ) => Promise<UserResult>
-);
-
-userController.setUpdateUserDetails((params: Partial<UserRequest>) => {
-  if ("userId" in params && "user" in params) {
-    return userService.updateUserByID(
-      params.userId as string,
-      params.user as Partial<UserRequest>
-    );
-  } else {
-    throw new Error("Invalid parameters");
-  }
-});
-
-userController.setUpdatePassword((params: Partial<UserRequest>) => {
-  if ("userId" in params && "user" in params) {
-    return userService.updateUserByID(
-      params.userId as string,
-      params.user as Partial<UserRequest>
-    );
-  } else {
-    throw new Error("Invalid parameters");
-  }
-});
+const controller = userController({} as UserServices);
 
 const registerHandler = (req: Request, res: Response) => {
-  const UserRequest: UserRequest = req.body;
-  userController.registerHandler(UserRequest, res);
+  const userRequest: UserRequest = req.body;
+  controller.registerHandler(userRequest, res);
 };
 
 const loginHandler = (req: Request, res: Response) => {
-  const UserRequest: UserRequest = req.body;
-  userController.loginHandler(UserRequest, res);
+  const loginRequest: LoginRequest = req.body;
+  controller.loginHandler(loginRequest, res);
 };
 
 const logoutHandler = (req: Request, res: Response) => {
-  userController.logoutHandler(req, res);
+  controller.logoutHandler(req, res);
 };
 
 const listUserHandler = (req: Request, res: Response) => {
   const userIdRequest: UserIdRequest = { userId: req.body.userId };
-  userController.listUserHandler(userIdRequest, res);
+  controller.listUserHandler(userIdRequest, res);
 };
 
 const updateUserDetailsHandler = (req: Request, res: Response) => {
-  userController.updateUserDetailsHandler(req as unknown as UpdateUserRequest, res);
+  const updateUserRequest: UpdateUserRequest = req.body;
+   controller.updateUserDetailsHandler(updateUserRequest, res);
 };
 
 const updateUserPasswordHandler = (req: Request, res: Response) => {
-  userController.updateUserPasswordHandler(req as unknown as UpdateUserRequest, res);
+  const updateUserRequest: UpdateUserRequest = req.body;
+  controller.updateUserPasswordHandler(updateUserRequest, res);
 };
 
 const deleteUserHandler = (req: Request, res: Response) => {
   const userIdRequest: UserIdRequest = { userId: req.body.userId };
-  userController.deleteUserHandler(userIdRequest, res);
+  controller.deleteUserHandler(userIdRequest, res);
 };
 
 router.post(
