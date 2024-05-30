bd6d561f7a84831212b4ff7d8267740316248413
Herbert Tamayo <hftamayo@gmail.com>
2024-05-30 13:00:50 -0600

    cleaning listUSer method of deprecated code

--- src/api/controllers/userController.ts
+++ src/api/controllers/userController.ts
@@ -99,16 +99,6 @@ export default function userController(userService: UserServices) {
 
     listUserHandler: async function (req: UserIdRequest, res: Response) {
       try {
-        // if (!req?.userId) {
-        //   res.status(400).json({
-        //     httpStatusCode: 400,
-        //     resultMessage: "Internal Error: User ID is required",
-        //   });
-        //   return;
-        // } else {
-        //   console.log("el ID del user a listar es: ", req.userId);
-        // }
-
         const result: UserResult = await userService.listUserByID(req);
         const { httpStatusCode, message, user } = result;
         if (httpStatusCode === 200 && user) {
