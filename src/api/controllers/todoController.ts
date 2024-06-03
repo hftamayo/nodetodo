c6bf12fe1c25691f87f7575ba7141aa7a62efea8
Herbert Tamayo <hftamayo@gmail.com>
2024-06-03 12:56:18 -0600

    cleaning getTodosHandler method

--- src/api/controllers/todoController.ts
+++ src/api/controllers/todoController.ts
@@ -1,207 +1,169 @@
-import { Response } from "express";
+import { Request, Response } from "express";
 
 import {
   TodoRequest,
   TodoIdRequest,
   OwnerTodoIdRequest,
-  TodoResult,
   OwnerTodoBodyRequest,
+  TodoResult,
+  TodoServices,
 } from "../../types/todo.interface";
 import { UserIdRequest } from "../../types/user.interface";
 
-let listActiveTodos: (newActiveTodos: UserIdRequest) => Promise<TodoResult>;
-let listTodoByID: (
-  params: UserIdRequest,
-  todoIdRequest: TodoIdRequest
-) => Promise<TodoResult>;
-let createTodo: (
-  params: UserIdRequest,
-  requestBody: Partial<TodoRequest>
-) => Promise<TodoResult>;
-let updateTodoByID: (
-  params: UserIdRequest,
-  requestTodoId: TodoIdRequest,
-  requestBody: Partial<TodoRequest>
-) => Promise<TodoResult>;
-let deleteTodoByID: (
-  userId: UserIdRequest,
-  todoId: TodoIdRequest
-) => Promise<TodoResult>;
+export default function todoController(todoService: TodoServices) {
+  return {
+    getTodosHandler: async function (req: UserIdRequest, res: Response) {
+      try {
+        const result: TodoResult = await todoService.listActiveTodos(req);
+        const { httpStatusCode, message, todos } = result;
+        res
+          .status(httpStatusCode)
+          .json(
+            httpStatusCode === 200
+              ? { httpStatusCode, resultMessage: message, activeTodos: todos }
+              : { httpStatusCode, resultMessage: message }
+          );
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          console.error("todoController, getTodos: " + error.message);
+        } else {
+          console.error("todoController, getTodos: " + error);
+        }
+        res
+          .status(500)
+          .json({
+            httpStatusCode: 500,
+            resultMessage: "Internal Server Error",
+          });
+      }
+    },
 
-const todoController = {
-  setActiveTodos: function (
-    newActiveTodos: (newActiveTodos: UserIdRequest) => Promise<TodoResult>
-  ) {
-    listActiveTodos = newActiveTodos;
-  },
-  setTodoByID: function (
-    newTodoByID: (
-      params: UserIdRequest,
-      todoIdRequest: TodoIdRequest
-    ) => Promise<TodoResult>
-  ) {
-    listTodoByID = newTodoByID;
-  },
-  setCreateTodo: function (
-    newCreateTodo: (
-      params: UserIdRequest,
-      requestBody: Partial<TodoRequest>
-    ) => Promise<TodoResult>
-  ) {
-    createTodo = newCreateTodo;
-  },
-  setUpdateTodoByID: function (
-    newUpdateTodoByID: (
-      params: UserIdRequest,
-      requestTodoId: TodoIdRequest,
-      requestBody: Partial<TodoRequest>
-    ) => Promise<TodoResult>
-  ) {
-    updateTodoByID = newUpdateTodoByID;
-  },
-  setDeleteTodoByID: function (
-    newDeleteTodoByID: (
-      userId: UserIdRequest,
-      todoId: TodoIdRequest
-    ) => Promise<TodoResult>
-  ) {
-    deleteTodoByID = newDeleteTodoByID;
-  },
-
-  getTodosHandler: async function (req: UserIdRequest, res: Response) {
-    try {
-      const { httpStatusCode, message, todos } = await listActiveTodos(req);
-      res
-        .status(httpStatusCode)
-        .json(
-          httpStatusCode === 200
-            ? { httpStatusCode, resultMessage: message, activeTodos: todos }
-            : { httpStatusCode, resultMessage: message }
+    getTodoHandler: async function (req: OwnerTodoIdRequest, res: Response) {
+      try {
+        const { httpStatusCode, message, todo } = await todoService.listTodoByID(
+          req.user,
+          req.params.id
         );
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        console.error("todoController, getTodos: " + error.message);
-      } else {
-        console.error("todoController, getTodos: " + error);
+        res
+          .status(httpStatusCode)
+          .json(
+            httpStatusCode === 200
+              ? { httpStatusCode, resultMessage: message, searchTodo: todo }
+              : { httpStatusCode, resultMessage: message }
+          );
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          console.error("todoController, getTodo: " + error.message);
+        } else {
+          console.error("todoController, getTodo: " + error);
+        }
+        res
+          .status(500)
+          .json({
+            httpStatusCode: 500,
+            resultMessage: "Internal Server Error",
+          });
       }
-      res
-        .status(500)
-        .json({ httpStatusCode: 500, resultMessage: "Internal Server Error" });
-    }
-  },
+    },
 
-  getTodoHandler: async function (req: OwnerTodoIdRequest, res: Response) {
-    try {
-      const { httpStatusCode, message, todo } = await listTodoByID(
-        req.user,
-        req.params.id
-      );
-      res
-        .status(httpStatusCode)
-        .json(
-          httpStatusCode === 200
-            ? { httpStatusCode, resultMessage: message, searchTodo: todo }
-            : { httpStatusCode, resultMessage: message }
+    newTodoHandler: async function (req: OwnerTodoBodyRequest, res: Response) {
+      try {
+        const { httpStatusCode, message, todo } = await todoService.createTodo(
+          req.owner,
+          req.todo
         );
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        console.error("todoController, getTodo: " + error.message);
-      } else {
-        console.error("todoController, getTodo: " + error);
+
+        if (
+          !req.todo.title ||
+          !req.todo.description ||
+          !req.todo.completed ||
+          !req.todo.user
+        ) {
+          return res.status(400).json({
+            httpStatusCode: 400,
+            resultMessage: "Missing required fields",
+          });
+        }
+
+        res
+          .status(httpStatusCode)
+          .json(
+            httpStatusCode === 200
+              ? { httpStatusCode, resultMessage: message, newTodo: todo }
+              : { httpStatusCode, resultMessage: message }
+          );
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          console.error("todoController, newTodo: " + error.message);
+        } else {
+          console.error("todoController, newTodo: " + error);
+        }
+        res
+          .status(500)
+          .json({
+            httpStatusCode: 500,
+            resultMessage: "Internal Server Error",
+          });
       }
-      res
-        .status(500)
-        .json({ httpStatusCode: 500, resultMessage: "Internal Server Error" });
-    }
-  },
+    },
 
-  newTodoHandler: async function (req: OwnerTodoBodyRequest, res: Response) {
-    try {
-      const { httpStatusCode, message, todo } = await createTodo(
-        req.owner,
-        req.todo
-      );
-
-      if (
-        !req.todo.title ||
-        !req.todo.description ||
-        !req.todo.completed ||
-        !req.todo.user
-      ) {
-        return res.status(400).json({
-          httpStatusCode: 400,
-          resultMessage: "Missing required fields",
-        });
-      }
-
-      res
-        .status(httpStatusCode)
-        .json(
-          httpStatusCode === 200
-            ? { httpStatusCode, resultMessage: message, newTodo: todo }
-            : { httpStatusCode, resultMessage: message }
+    updateTodoHandler: async function (
+      req: OwnerTodoBodyRequest,
+      res: Response
+    ) {
+      try {
+        if (!req.todoId) {
+          throw new Error("Missing todoId");
+        }
+        const { httpStatusCode, message, todo } = await todoService.updateTodoByID(
+          req.owner,
+          req.todoId,
+          req.todo
         );
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        console.error("todoController, newTodo: " + error.message);
-      } else {
-        console.error("todoController, newTodo: " + error);
+        res
+          .status(httpStatusCode)
+          .json(
+            httpStatusCode === 200
+              ? { httpStatusCode, resultMessage: message, updateTodo: todo }
+              : { httpStatusCode, resultMessage: message }
+          );
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          console.error("todoController, updateTodo: " + error.message);
+        } else {
+          console.error("todoController, updateTodo: " + error);
+        }
+        res
+          .status(500)
+          .json({
+            httpStatusCode: 500,
+            resultMessage: "Internal Server Error",
+          });
       }
-      res
-        .status(500)
-        .json({ httpStatusCode: 500, resultMessage: "Internal Server Error" });
-    }
-  },
+    },
 
-  updateTodoHandler: async function (req: OwnerTodoBodyRequest, res: Response) {
-    try {
-      if (!req.todoId) {
-        throw new Error("Missing todoId");
-      }
-      const { httpStatusCode, message, todo } = await updateTodoByID(
-        req.owner,
-        req.todoId,
-        req.todo
-      );
-      res
-        .status(httpStatusCode)
-        .json(
-          httpStatusCode === 200
-            ? { httpStatusCode, resultMessage: message, updateTodo: todo }
-            : { httpStatusCode, resultMessage: message }
+    deleteTodoHandler: async function (req: OwnerTodoIdRequest, res: Response) {
+      try {
+        const { httpStatusCode, message } = await todoService.deleteTodoByID(
+          req.user,
+          req.params.id
         );
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        console.error("todoController, updateTodo: " + error.message);
-      } else {
-        console.error("todoController, updateTodo: " + error);
+        res
+          .status(httpStatusCode)
+          .json({ httpStatusCode, resultMessage: message });
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          console.error("todoController, deleteTodo: " + error.message);
+        } else {
+          console.error("todoController, deleteTodo: " + error);
+        }
+        res
+          .status(500)
+          .json({
+            httpStatusCode: 500,
+            resultMessage: "Internal Server Error",
+          });
       }
-      res
-        .status(500)
-        .json({ httpStatusCode: 500, resultMessage: "Internal Server Error" });
-    }
-  },
-
-  deleteTodoHandler: async function (req: OwnerTodoIdRequest, res: Response) {
-    try {
-      const { httpStatusCode, message } = await deleteTodoByID(
-        req.user,
-        req.params.id
-      );
-      res
-        .status(httpStatusCode)
-        .json({ httpStatusCode, resultMessage: message });
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        console.error("todoController, deleteTodo: " + error.message);
-      } else {
-        console.error("todoController, deleteTodo: " + error);
-      }
-      res
-        .status(500)
-        .json({ httpStatusCode: 500, resultMessage: "Internal Server Error" });
-    }
-  },
-};
-
-export default todoController;
+    },
+  };
+}
