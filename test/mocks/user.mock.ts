301dc1c95dcaf98e93ca3a338736a5346a547679
Herbert Tamayo <hftamayo@gmail.com>
2024-06-20 15:02:30 -0600

    new version of user mock module

---
+++ test/mocks/user.mock.ts
@@ -0,0 +1,9 @@
+import { mongo } from 'mongoose';
+import User from '../../src/models/User';
+
+export const mockUserRoleUser = {
+    name: 'John Doe',
+    email: 'john@example.com',
+    password: 'securePassword123',
+    age: 30,
+};
