bbc5fc178be073050d017768550ecad7b06c77a3
Herbert Tamayo <hftamayo@gmail.com>
2024-06-25 13:24:27 -0600

    adding todoSup case

--- test/mocks/todo.mock.ts
+++ test/mocks/todo.mock.ts
@@ -1,14 +1,20 @@
-import Todo from "../../src/models/Todo";
-import User from "../../src/models/User";
 import mongoose from "mongoose";
 
 const getRandomInt = (max: number) => {
     return Math.floor(Math.random() * Math.floor(max));
   };
 
-const newTodo = (userId = new mongoose.Types.ObjectId()) => ({
+export const newTodo = (userId = new mongoose.Types.ObjectId()) => ({
     title: `New Todo${getRandomInt(1000000)}`,
     description: "New Todo Description",
     completed: false,
     user: userId,
 });
+
+export const todoSupervisor = {
+  id : new mongoose.Types.ObjectId(),
+  title: "Gym",
+  description: "To exercise",
+  completed: false,
+  user: "5f7f8b1e9f3f9c1d6c1e4d1f",
+};
