02bcd7fa267fbe1ceb838d849fb8c358af2fddc8
Herbert Tamayo <hftamayo@gmail.com>
2024-06-25 14:03:08 -0600

    simplifying the object newStandardTodo mock

--- test/mocks/todo.mock.ts
+++ test/mocks/todo.mock.ts
@@ -1,18 +1,19 @@
 import mongoose from "mongoose";
 
 const getRandomInt = (max: number) => {
-    return Math.floor(Math.random() * Math.floor(max));
-  };
+  return Math.floor(Math.random() * Math.floor(max));
+};
 
-export const newStandardTodo = (userId = new mongoose.Types.ObjectId()) => ({
-    title: `New Todo${getRandomInt(1000000)}`,
-    description: "New Todo Description",
-    completed: false,
-    user: userId,
-});
+export const newStandardTodo = {
+  id: new mongoose.Types.ObjectId(),
+  title: `New Todo${getRandomInt(1000000)}`,
+  description: "New Todo Description",
+  completed: false,
+  user: new mongoose.Types.ObjectId(),
+};
 
 export const todoSupervisor = {
-  id : new mongoose.Types.ObjectId(),
+  id: new mongoose.Types.ObjectId(),
   title: "Gym",
   description: "To exercise",
   completed: false,
