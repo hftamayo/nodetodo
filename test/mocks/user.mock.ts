bd44a09bf705bb7436bc2eaecee4ecd36d0812f2
Herbert Tamayo <hftamayo@gmail.com>
2024-06-26 10:37:09 -0600

    adding more mock cases for user unit tests

--- test/mocks/user.mock.ts
+++ test/mocks/user.mock.ts
@@ -1,9 +1,37 @@
 import { mongo } from 'mongoose';
 import User from '../../src/models/User';
 
+const getRandomInt = (max: number) => {
+    return Math.floor(Math.random() * Math.floor(max));
+  };
+
 export const mockUserRoleUser = {
     name: 'John Doe',
     email: 'john@example.com',
     password: 'securePassword123',
     age: 30,
 };
+
+export const mockUserInvalid = {
+    id: "123123123AAA",
+    name: "Herbert Fernandez",
+    email: "hftamayo2@gmail.com",
+    password: "incorrect",
+    age: 50,
+  };
+
+  export const mockUserUpdate = {
+    name: "Yuki Fernandez Martinez",
+    email: `yuki${getRandomInt(1000000)}@gmail.com`,
+    emailTaken: "hftamayo@gmail.com",
+    oldPassword: "password",
+    newPassword: "milucito",
+    notMatchPassword: "password2",
+    age: 20,
+  };
+
+  //used just in unit testing
+const mockUserDelete = {
+    id: "222222222",
+  };
+  
