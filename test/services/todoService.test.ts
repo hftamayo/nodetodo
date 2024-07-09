c7d344659a2ab6a76fb9a91b4d6e00361f6ddeb6
Herbert Tamayo <hftamayo@gmail.com>
2024-07-09 10:26:33 -0600

    adjusting todoService path on mock stub

--- test/services/todoService.test.ts
+++ test/services/todoService.test.ts
@@ -14,7 +14,7 @@ import {
 import { UserIdRequest } from "../../src/types/user.interface";
 import todoService from "../../src/services/todoService";
 
-jest.mock('../path/to/todoService', () => ({
+jest.mock('../../src/services/todoService', () => ({
   listTodoByID: jest.fn().mockResolvedValue({
     httpStatusCode: 200,
     message: "Todo found",
