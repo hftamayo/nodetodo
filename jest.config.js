8d02ea0284136aa0ff2acad33c89109b0a0225d1
Herbert Tamayo <hftamayo@gmail.com>
2024-12-31 07:51:15 -0600

    tweaking tsconfig and jest config to avoid including extension on import

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 module.exports = {
+  roots: ["<rootDir>/src", "<rootDir>/test"],
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["<rootDir>/test/**/*.test.ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
 };
