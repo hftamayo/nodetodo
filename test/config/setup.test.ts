76d74c5f94c0e68af26a520bbe1969600d884116
Herbert Tamayo <hftamayo@gmail.com>
2024-12-31 08:07:27 -0600

    giving another approach to setup test

--- test/config/setup.test.ts
+++ test/config/setup.test.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import { dbConnection, setCorsEnviro } from "../../src/config/setup";
-import { backend, whitelist_frontend } from "../../src/config/envvars";
+import * as envvars from "../../src/config/envvars";
 
 jest.mock("mongoose");
 
@@ -10,33 +10,25 @@ describe("dbConnection", () => {
   });
 
   it("should throw an error if backend URL is not provided", async () => {
-    const originalBackend = backend;
-    jest
-      .spyOn(require("../../src/config/setup"), "backend", "get")
-      .mockReturnValue(undefined);
+    const originalBackend = envvars.backend;
+    jest.spyOn(envvars, "backend", "get").mockReturnValue(undefined);
 
     await expect(dbConnection()).rejects.toThrow("Backend URL not found");
 
-    jest
-      .spyOn(require("../../src/config/setup"), "backend", "get")
-      .mockReturnValue(originalBackend);
+    jest.spyOn(envvars, "backend", "get").mockReturnValue(originalBackend);
   });
 
   it("should attempt to connect to the database using the provided backend URL", async () => {
-    const originalBackend = backend;
+    const originalBackend = envvars.backend;
     const testBackendUrl = "mongodb://localhost:27017/test";
 
-    jest
-      .spyOn(require("../../src/config/setup"), "backend", "get")
-      .mockReturnValue(testBackendUrl);
+    jest.spyOn(envvars, "backend", "get").mockReturnValue(testBackendUrl);
 
     await dbConnection();
 
     expect(mongoose.connect).toHaveBeenCalledWith(testBackendUrl);
 
-    jest
-      .spyOn(require("../../src/config/setup"), "backend", "get")
-      .mockReturnValue(originalBackend);
+    jest.spyOn(envvars, "backend", "get").mockReturnValue(originalBackend);
   });
 
   it("should log an error and exit the process if the connection fails", async () => {
@@ -62,7 +54,7 @@ describe("dbConnection", () => {
 
 describe("setCorsEnviro", () => {
   it("should allow requests from whitelisted origins", () => {
-    const origin = whitelist_frontend[0];
+    const origin = envvars.whitelist_frontend[0];
     const callback = jest.fn();
 
     setCorsEnviro.origin(origin, callback);
