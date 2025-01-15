bb03ccb8294272fc22aba8d032f6d85469081470
Herbert Tamayo <hftamayo@gmail.com>
2025-01-15 10:27:20 -0600

    improving data seeding with enviro pwords

--- src/utils/seedUsers.ts
+++ src/utils/seedUsers.ts
@@ -2,29 +2,34 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import User from "../models/User";
 import { UserRole, FullUser } from "../types/user.types";
+import { adminpword, supervisorpword, userpword } from "@/config/envvars";
+
+if (!adminpword || !supervisorpword || !userpword) {
+  throw new Error("No passwords found in the environment variables");
+}
 
 const users: FullUser[] = [
   {
     _id: new mongoose.Types.ObjectId("5f7f8b1e9f3f9c1d6c1e4d1e"),
     name: "Administrator",
-    email: "administrator@nodetodo.com",
-    password: "password",
+    email: "administrador@tamayo.com",
+    password: adminpword,
     age: 30,
     role: UserRole.ADMIN,
   },
   {
     _id: new mongoose.Types.ObjectId("5f7f8b1e9f3f9c1d6c1e4d1f"),
     name: "Sebastian Fernandez",
-    email: "sebas@gmail.com",
-    password: "password",
+    email: "supervisor@tamayo.com",
+    password: supervisorpword,
     age: 20,
     role: UserRole.SUPERVISOR,
   },
   {
     _id: new mongoose.Types.ObjectId("5f7f8b1e9f3f9c1d6c1e4d20"),
-    name: "Lupita Martinez",
-    email: "lupita@fundamuvi.com",
-    password: "password",
+    name: "Bob Doe",
+    email: "bob@tamayo.com",
+    password: userpword,
     age: 25,
     role: UserRole.USER,
   },
