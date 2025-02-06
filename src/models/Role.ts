f2ccbaade3c410eab082da929eb66f92ecebfaf4
Herbert Tamayo <hftamayo@gmail.com>
2025-02-06 13:37:41 -0600

    adding timestamps

--- src/models/Role.ts
+++ src/models/Role.ts
@@ -1,25 +1,28 @@
 import mongoose from "mongoose";
 
-const RoleSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
+const RoleSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: Boolean,
+      required: true,
+      default: true,
+    },
+    permissions: {
+      type: [String],
+      required: true,
+    },
   },
-  description: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: Boolean,
-    required: true,
-    default: true,
-  },
-  permissions: {
-    type: [String],
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const Role = mongoose.model("Role", RoleSchema);
 
