02c4959241171fc70be043d03f74144766f80ce4
Herbert Tamayo <hftamayo@gmail.com>
2025-02-07 10:32:32 -0600

    role seeding

---
+++ src/utils/seedRoles.ts
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import Role from "../models/Role";
+import { FullRole } from "@/types/role.types";
+
+const roles: FullRole[] = [
+  {
+    _id: new mongoose.Types.ObjectId("5f7f8b1e9f3f9c1d6c1e4d1f"),
+    name: "admin",
+    description: "Admin role",
+    status: true,
+    permissions: ["read", "write", "delete"],
+  },
+  {
+    _id: new mongoose.Types.ObjectId("5f7f8b1e9f3f9c1d6c1e4d1f"),
+    name: "supervisor",
+    description: "Supervisor role",
+    status: true,
+    permissions: ["read", "delete"],
+  },
+  {
+    _id: new mongoose.Types.ObjectId("5f7f8b1e9f3f9c1d6c1e4d20"),
+    name: "user",
+    description: "User role",
+    status: true,
+    permissions: ["read", "write"],
+  },
+];
+
+async function seedRoles() {
+  try {
+    await Role.deleteMany({});
+    for (const role of roles) {
+      await Role.create(role);
+      console.log("Role created: ", role);
+    }
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error("seedRoles: ", error.message);
+    } else {
+      console.error("seedRoles: ", error);
+    }
+  }
+}
+
+export default seedRoles;
