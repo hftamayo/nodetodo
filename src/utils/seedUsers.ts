6132bc27ba41583064d22eed4e91298c3dcf5b38
Herbert Tamayo <hftamayo@gmail.com>
2025-02-07 10:40:28 -0600

    updating user data seeding

--- src/utils/seedUsers.ts
+++ src/utils/seedUsers.ts
@@ -1,50 +1,59 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import User from "../models/User";
-import { UserRole, FullUser } from "../types/user.types";
+import Role from "../models/Role";
+import { FullUser } from "../types/user.types";
 import { adminpword, supervisorpword, userpword } from "../config/envvars";
 
 if (!adminpword || !supervisorpword || !userpword) {
   throw new Error("No passwords found in the environment variables");
 }
 
-const users: FullUser[] = [
-  {
-    _id: new mongoose.Types.ObjectId("5f7f8b1e9f3f9c1d6c1e4d1e"),
-    name: "Administrator",
-    email: "administrador@tamayo.com",
-    password: adminpword,
-    age: 30,
-    role: UserRole.ADMIN,
-    status: true,
-  },
-  {
-    _id: new mongoose.Types.ObjectId("5f7f8b1e9f3f9c1d6c1e4d1f"),
-    name: "Sebastian Fernandez",
-    email: "supervisor@tamayo.com",
-    password: supervisorpword,
-    age: 20,
-    role: UserRole.SUPERVISOR,
-    status: true,
-  },
-  {
-    _id: new mongoose.Types.ObjectId("5f7f8b1e9f3f9c1d6c1e4d20"),
-    name: "Bob Doe",
-    email: "bob@tamayo.com",
-    password: userpword,
-    age: 25,
-    role: UserRole.USER,
-    status: true,
-  },
-];
-
 async function seedUsers() {
   try {
+    // Ensure roles are seeded first
+    const adminRole = await Role.findOne({ name: "admin" }).exec();
+    const supervisorRole = await Role.findOne({ name: "supervisor" }).exec();
+    const userRole = await Role.findOne({ name: "user" }).exec();
+
+    if (!adminRole || !supervisorRole || !userRole) {
+      throw new Error("Roles not found in the database");
+    }
+
+    const users: FullUser[] = [
+      {
+        _id: new mongoose.Types.ObjectId("5f7f8b1e9f3f9c1d6c1e4d1e"),
+        name: "Administrator",
+        email: "administrador@tamayo.com",
+        password: adminpword!,
+        age: 30,
+        role: adminRole._id,
+        status: true,
+      },
+      {
+        _id: new mongoose.Types.ObjectId("5f7f8b1e9f3f9c1d6c1e4d1f"),
+        name: "Sebastian Fernandez",
+        email: "supervisor@tamayo.com",
+        password: supervisorpword!,
+        age: 20,
+        role: supervisorRole._id,
+        status: true,
+      },
+      {
+        _id: new mongoose.Types.ObjectId("5f7f8b1e9f3f9c1d6c1e4d20"),
+        name: "Bob Doe",
+        email: "bob@tamayo.com",
+        password: userpword!,
+        age: 25,
+        role: userRole._id,
+        status: true,
+      },
+    ];
+
     await User.deleteMany({});
     for (const user of users) {
       const salt = await bcrypt.genSalt(10);
-
-      user.password = await bcrypt.hash("password", salt);
+      user.password = await bcrypt.hash(user.password, salt);
       await User.create(user);
       console.log("User created: ", user);
     }
