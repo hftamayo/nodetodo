ef4cbca6043703d36adb2e572b40b8872f1dcbc3
Herbert Tamayo <hftamayo@gmail.com>
2025-02-10 12:50:49 -0600

    role service first version

--- src/services/roleService.ts
+++ src/services/roleService.ts
@@ -1,5 +1,6 @@
 import {
   CreateRoleResponse,
+  DeleteRoleResponse,
   FilteredRole,
   FullRole,
   ListRoleResponse,
@@ -7,6 +8,8 @@ import {
   ListRolesResponse,
   NewRoleRequest,
   RoleIdRequest,
+  UpdateRoleRequest,
+  UpdateRoleResponse,
 } from "@/types/role.types";
 import Role from "../models/Role";
 
@@ -116,4 +119,80 @@ const createRole = async function (
     }
     return { httpStatusCode: 500, message: "UNKNOWN_ERROR" };
   }
+};
+
+const updateRoleByID = async function (
+  params: UpdateRoleRequest
+): Promise<UpdateRoleResponse> {
+  const { roleId, role } = params;
+  const { name, description, status, permissions } = role;
+
+  if (!name || !description || !status || !permissions) {
+    return { httpStatusCode: 400, message: "MISSING_FIELDS" };
+  }
+
+  try {
+    let searchRole = await Role.findById(roleId).exec();
+
+    if (!searchRole) {
+      return { httpStatusCode: 404, message: "ENTITY_NOT_FOUND" };
+    }
+
+    searchRole.name = name;
+    searchRole.description = description;
+    searchRole.status = status;
+    searchRole.permissions = permissions;
+
+    await searchRole.save();
+
+    const filteredRole: FilteredRole = {
+      _id: searchRole._id,
+      name: searchRole.name,
+      description: searchRole.description,
+      status: searchRole.status,
+      permissions: searchRole.permissions,
+    };
+
+    return {
+      httpStatusCode: 200,
+      message: "ROLE_UPDATED",
+      role: filteredRole,
+    };
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error("roleService, updateRoleByID: " + error.message);
+    } else {
+      console.error("roleService, updateRoleByID: " + error);
+    }
+    return { httpStatusCode: 500, message: "UNKNOWN_ERROR" };
+  }
+};
+
+const deleteRoleByID = async function (
+  params: RoleIdRequest
+): Promise<DeleteRoleResponse> {
+  const roleId = params.roleId;
+  try {
+    let searchRole = await Role.findById(roleId).exec();
+    if (!searchRole) {
+      return { httpStatusCode: 404, message: "ENTITY_NOT_FOUND" };
+    }
+    await searchRole.deleteOne();
+    return { httpStatusCode: 200, message: "ENTITY_DELETED" };
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error("roleService, deleteRoleByID: " + error.message);
+    } else {
+      console.error("roleService, deleteRoleByID: " + error);
+    }
+    return { httpStatusCode: 500, message: "UNKNOWN_ERROR" };
+  }
+};
+
+export default {
+  listRoles,
+  listRoleByID,
+  createRole,
+  updateRoleByID,
+  deleteRoleByID,
 };
