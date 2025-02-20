4a30de431429724deb77e2ceea347d861ce3016a
Herbert Tamayo <hftamayo@gmail.com>
2025-02-20 13:30:33 -0600

    updating the workflow of task data seeding

--- src/utils/seedTodos.ts
+++ src/utils/seedTodos.ts
@@ -1,18 +1,28 @@
 import mongoose from "mongoose";
 import Todo from "../models/Todo";
 import { FullTodo } from "../types/todo.types";
-
-const todos: Omit<FullTodo, "_id">[] = [
-  {
-    title: "Foreign language class",
-    description: "To learn a new language",
-    completed: false,
-    owner: new mongoose.Types.ObjectId("5f7f8b1e9f3f9c1d6c1e4d1e"),
-  },
-];
+import seedUsers from "./seedUsers";
 
 async function seedTodos(session: mongoose.ClientSession) {
   try {
+    const users = await seedUsers(session);
+    if (!users) {
+      throw new Error("Users not found in the database");
+    }
+    const userBob = users.find((user) => user.email === "bob@tamayo.com");
+    if (!userBob) {
+      throw new Error("User Bob not found in the database");
+    }
+
+    const todos: Omit<FullTodo, "_id">[] = [
+      {
+        title: "Foreign language class",
+        description: "To learn a new language",
+        completed: false,
+        owner: userBob._id,
+      },
+    ];
+
     await Todo.deleteMany({}).session(session);
     const todosCreated = await Todo.create([todos], { session });
     console.log("Todos created: ", todosCreated);
