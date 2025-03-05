6a449edd01bda44ae0372138fd87d00efa38183e
Herbert Tamayo <hftamayo@gmail.com>
2025-03-05 10:21:13 -0600

    adding type safety validation for the authUser type

--- src/api/middleware/validateAuth.ts
+++ src/api/middleware/validateAuth.ts
@@ -1,12 +1,18 @@
 import { Response, NextFunction } from "express";
 import { AuthenticatedUserRequest } from "../../types/user.types";
 
+function isAuthenticated(
+  req: AuthenticatedUserRequest
+): req is AuthenticatedUserRequest & { user: { id: string; role: string } } {
+  return req.user !== undefined;
+}
+
 const validateAuthentication = (
   req: AuthenticatedUserRequest,
   res: Response,
   next: NextFunction
 ) => {
-  if (!req.user?.id) {
+  if (!isAuthenticated(req)) {
     return res.status(401).json({ code: 401, resultMessage: "NOT_AUTHORIZED" });
   }
   next();
