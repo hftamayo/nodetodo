24ada9aa25fa851e5e6579f69ebc2c41142f4dad
Herbert Tamayo <hftamayo@gmail.com>
2025-03-07 10:31:07 -0600

    first version of error log module

---
+++ src/utils/error/errorLog.ts
@@ -0,0 +1,23 @@
+import { ApiError, ErrorType, ErrorTypes } from "@/types/error.types";
+import { mode } from "@/config/envvars";
+
+export const createApiError = (
+  type: ErrorType,
+  message: string,
+  debug?: string
+): ApiError => {
+  const error: ApiError = {
+    code: ErrorTypes[type].code,
+    resultMessage: ErrorTypes[type].message,
+    debugMessage: debug ? message + ": " + debug : message,
+  };
+
+  // Log error details in development
+  if (mode !== "production") {
+    console.error(
+      `[${error.code}][${error.resultMessage}] ${error.debugMessage}`
+    );
+  }
+
+  return error;
+};
