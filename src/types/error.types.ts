7840d1442d1fab59c1f9f03b0c3d14a8c580fafd
Herbert Tamayo <hftamayo@gmail.com>
2025-03-07 10:27:06 -0600

    adding error types for errorLog routine

---
+++ src/types/error.types.ts
@@ -0,0 +1,30 @@
+export type ApiError = {
+  code: number;
+  resultMessage: string;
+  debugMessage?: string;
+};
+
+export const ErrorTypes = {
+  NOT_AUTHORIZED: {
+    code: 401,
+    message: "NOT_AUTHORIZED",
+  },
+  FORBIDDEN: {
+    code: 403,
+    message: "FORBIDDEN",
+  },
+  NOT_FOUND: {
+    code: 404,
+    message: "NOT_FOUND",
+  },
+  BAD_REQUEST: {
+    code: 400,
+    message: "BAD_REQUEST",
+  },
+  INTERNAL_ERROR: {
+    code: 500,
+    message: "INTERNAL_ERROR",
+  },
+} as const;
+
+type ErrorType = keyof typeof ErrorTypes;
