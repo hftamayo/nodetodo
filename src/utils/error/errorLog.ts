94738a66ca8fcf26be37afa4efbb5333e3a98292
Herbert Tamayo <hftamayo@gmail.com>
2025-03-13 11:01:37 -0600

    improving errorLog scope

--- src/utils/error/errorLog.ts
+++ src/utils/error/errorLog.ts
@@ -4,9 +4,20 @@ import { mode } from "../../config/envvars";
 export const createApiError = (
   type: ErrorType,
   message: string,
-  debug?: string
+  debug?: string,
+  path?: string,
+  method?: string,
+  domain?: string,
+  requiredPermission?: string,
+  token?: string
 ): ApiError => {
   const error: ApiError = {
+    timestamp: new Date().toISOString(),
+    path: path?.toString(),
+    method: method?.toString(),
+    domain: domain?.toString(),
+    requiredPermission: requiredPermission?.toString(),
+    cookiePresent: !!token,
     code: ErrorTypes[type].code,
     resultMessage: ErrorTypes[type].message,
     debugMessage: debug ? message + ": " + debug : message,
@@ -14,9 +25,22 @@ export const createApiError = (
 
   // Log error details in development
   if (mode !== "production") {
-    console.error(
-      `[${error.code}][${error.resultMessage}] ${error.debugMessage}`
-    );
+    console.error(`
+      [ERROR LOG]
+      Timestamp: ${error.timestamp}
+      Path: ${error.path ?? "N/A"},
+      Method: ${error.method ?? "N/A"},
+      Domain: ${error.domain ?? "N/A"},
+      Required Permission: ${error.requiredPermission ?? "N/A"},
+      Cookie Present: ${error.cookiePresent}
+      Code: ${error.code}
+      Type: ${type}
+      Message: ${error.resultMessage}
+      Debug: ${error.debugMessage}
+      Stack: ${new Error().stack}
+    `);
+  } else {
+    //persistence routine for error logging
   }
 
   return error;
