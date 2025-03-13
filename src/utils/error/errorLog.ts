382be10b026ee6ab65c19e3849f25b0d18ea6496
Herbert Tamayo <hftamayo@gmail.com>
2025-03-13 11:08:54 -0600

    applying new error types

--- src/utils/error/errorLog.ts
+++ src/utils/error/errorLog.ts
@@ -1,26 +1,31 @@
-import { ApiError, ErrorType, ErrorTypes } from "../../types/error.types";
+import {
+  ApiError,
+  ErrorContext,
+  ErrorType,
+  ErrorTypes,
+} from "../../types/error.types";
 import { mode } from "../../config/envvars";
 
 export const createApiError = (
   type: ErrorType,
   message: string,
   debug?: string,
-  path?: string,
-  method?: string,
-  domain?: string,
-  requiredPermission?: string,
-  token?: string
+  context?: ErrorContext
 ): ApiError => {
   const error: ApiError = {
-    timestamp: new Date().toISOString(),
-    path: path?.toString(),
-    method: method?.toString(),
-    domain: domain?.toString(),
-    requiredPermission: requiredPermission?.toString(),
-    cookiePresent: !!token,
     code: ErrorTypes[type].code,
     resultMessage: ErrorTypes[type].message,
+    timestamp: new Date().toISOString(),
     debugMessage: debug ? message + ": " + debug : message,
+    context: context
+      ? {
+          path: context.path,
+          method: context.method,
+          domain: context.domain,
+          requiredPermission: context.requiredPermission,
+          cookiePresent: context.cookiePresent,
+        }
+      : undefined,
   };
 
   // Log error details in development
@@ -28,11 +33,11 @@ export const createApiError = (
     console.error(`
       [ERROR LOG]
       Timestamp: ${error.timestamp}
-      Path: ${error.path ?? "N/A"},
-      Method: ${error.method ?? "N/A"},
-      Domain: ${error.domain ?? "N/A"},
-      Required Permission: ${error.requiredPermission ?? "N/A"},
-      Cookie Present: ${error.cookiePresent}
+      Path: ${error.context?.path ?? "N/A"},
+      Method: ${error.context?.method ?? "N/A"},
+      Domain: ${error.context?.domain ?? "N/A"},
+      Required Permission: ${error.context?.requiredPermission ?? "N/A"},
+      Cookie Present: ${error.context?.cookiePresent}
       Code: ${error.code}
       Type: ${type}
       Message: ${error.resultMessage}
