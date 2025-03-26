89b78acac227949597500aa5e8d0978f03f5b200
Herbert Tamayo <hftamayo@gmail.com>
2025-03-26 10:28:07 -0600

    updating jest config with path aliases

--- jest.config.js
+++ jest.config.js
@@ -9,5 +9,13 @@ module.exports = {
   testMatch: ["<rootDir>/test/**/*.test.ts"],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
+    "^@types/(.*)$": "<rootDir>/src/types/$1",
+    "^@services/(.*)$": "<rootDir>/src/services/$1",
+    "^@models/(.*)$": "<rootDir>/src/models/$1",
+    "^@controllers/(.*)$": "<rootDir>/src/api/controllers/$1",
+    "^@routes/(.*)$": "<rootDir>/src/api/routes/$1",
+    "^@middleware/(.*)$": "<rootDir>/src/api/middleware/$1",
+    "^@utils/(.*)$": "<rootDir>/src/utils/$1",
+    "^@config/(.*)$": "<rootDir>/src/config/$1",
   },
 };
