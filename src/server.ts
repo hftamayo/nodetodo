1eedb53dde2325ad47e9cc3af0e366b5a83492bd
Herbert Tamayo <hftamayo@gmail.com>
2025-03-26 11:00:26 -0600

    moving server entry point file into src

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import { startApp } from "./app";
+
+startApp()
+  .then(() => {
+    console.log("Nodetodo backend is up and ready");
+  })
+  .catch((error: any) => {
+    console.error("Error starting Nodetodo backend: ", error);
+    process.exit(1);
+  });
