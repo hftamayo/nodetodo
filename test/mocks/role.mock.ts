1410163c7ffdbb9d9831966c38bfedc3503e833c
Herbert Tamayo <hftamayo@gmail.com>
2025-06-13 09:13:07 -0600

    polishing role mock props

--- test/mocks/role.mock.ts
+++ test/mocks/role.mock.ts
@@ -1,9 +1,26 @@
 import mongoose from "mongoose";
 import { FilteredRole } from "@/types/role.types";
 
+// Mock mongoose.Types.ObjectId
+jest.mock("mongoose", () => ({
+  ...jest.requireActual("mongoose"),
+  Types: {
+    ObjectId: jest.fn().mockImplementation((id) => ({
+      _id: id,
+      toString: () => id,
+      toHexString: () => id,
+      toJSON: () => id,
+      equals: (other: any) => other && other.toString() === id,
+      getTimestamp: () => new Date(),
+      toBSON: () => ({ _id: id }),
+      inspect: () => `ObjectId("${id}")`,
+    })),
+  },
+}));
+
 export const mockRolesData = [
   {
-    _id: new mongoose.Types.ObjectId(),
+    _id: new mongoose.Types.ObjectId("adminRoleId123456789012"),
     name: "admin",
     description: "Administrator role",
     status: true,
@@ -13,7 +30,7 @@ export const mockRolesData = [
     ]),
   },
   {
-    _id: new mongoose.Types.ObjectId(),
+    _id: new mongoose.Types.ObjectId("userRoleId123456789012"),
     name: "user",
     description: "Basic user role",
     status: true,
