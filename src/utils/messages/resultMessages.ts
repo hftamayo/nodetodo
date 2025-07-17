d23d80a2db802f1ce15f6c044c0b590e59aa4b9f
Herbert Tamayo <hftamayo@gmail.com>
2025-07-17 09:34:43 -0600

    improving messages management

---
+++ src/utils/messages/resultMessages.ts
@@ -0,0 +1,13 @@
+export const ResultMessages = {
+  SUCCESS: { code: 201, message: "ENTITY_CREATED" },
+  OK: { code: 200, message: "OPERATION_SUCCESS" },
+  ERROR: { code: 404, message: "ENTITY_NOT_FOUND" },
+  BAD_REQUEST: { code: 400, message: "REQUIREMENT_MISSING" },
+  UNAUTHORIZED: { code: 401, message: "UNAUTHORIZED" },
+  FORBIDDEN: { code: 403, message: "FORBIDDEN" },
+  INTERNAL_SERVER_ERROR: { code: 500, message: "UNKNOWN_SERVER_ERROR" },
+  ROLE_ALREADY_EXISTS: { code: 400, message: "ROLE_ALREADY_EXISTS" },
+  // ...add more as needed
+} as const;
+
+export type ResultMessageKey = keyof typeof ResultMessages;
