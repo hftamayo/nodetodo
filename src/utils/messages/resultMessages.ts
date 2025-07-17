1a3534f482cb8078e9fd31c38996151e79d50fd2
Herbert Tamayo <hftamayo@gmail.com>
2025-07-17 09:58:31 -0600

    minor updates on keys

--- src/utils/messages/resultMessages.ts
+++ src/utils/messages/resultMessages.ts
@@ -1,12 +1,12 @@
 export const ResultMessages = {
-  SUCCESS: { code: 201, message: "ENTITY_CREATED" },
-  OK: { code: 200, message: "OPERATION_SUCCESS" },
+  CREATED: { code: 201, message: "ENTITY_CREATED" },
+  SUCCESS: { code: 200, message: "OPERATION_SUCCESS" },
   ERROR: { code: 404, message: "ENTITY_NOT_FOUND" },
   BAD_REQUEST: { code: 400, message: "REQUIREMENT_MISSING" },
   UNAUTHORIZED: { code: 401, message: "UNAUTHORIZED" },
   FORBIDDEN: { code: 403, message: "FORBIDDEN" },
   INTERNAL_SERVER_ERROR: { code: 500, message: "UNKNOWN_SERVER_ERROR" },
-  ROLE_ALREADY_EXISTS: { code: 400, message: "ROLE_ALREADY_EXISTS" },
+  ENTITY_ALREADY_EXISTS: { code: 400, message: "ROLE_ALREADY_EXISTS" },
   // ...add more as needed
 } as const;
 
