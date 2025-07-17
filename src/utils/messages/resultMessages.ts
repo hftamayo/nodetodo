03552897fdf95d05b340bf2d901a6936e8685323
Herbert Tamayo <hftamayo@gmail.com>
2025-07-17 10:38:57 -0600

    fixing minor bugs

--- src/utils/messages/resultMessages.ts
+++ src/utils/messages/resultMessages.ts
@@ -2,11 +2,11 @@ export const ResultMessages = {
   CREATED: { code: 201, message: "ENTITY_CREATED" },
   SUCCESS: { code: 200, message: "OPERATION_SUCCESS" },
   ERROR: { code: 404, message: "ENTITY_NOT_FOUND" },
-  BAD_REQUEST: { code: 400, message: "REQUIREMENT_MISSING" },
+  BAD_REQUEST: { code: 400, message: "REQUIREMENTS_MISSING" },
   UNAUTHORIZED: { code: 401, message: "UNAUTHORIZED" },
   FORBIDDEN: { code: 403, message: "FORBIDDEN" },
   INTERNAL_SERVER_ERROR: { code: 500, message: "UNKNOWN_SERVER_ERROR" },
-  ENTITY_ALREADY_EXISTS: { code: 400, message: "ROLE_ALREADY_EXISTS" },
+  ENTITY_ALREADY_EXISTS: { code: 400, message: "ENTITY_ALREADY_EXISTS" },
   // ...add more as needed
 } as const;
 
