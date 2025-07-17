21bb7628316d88c6656868ce3212b5d4f0f6ae23
Herbert Tamayo <hftamayo@gmail.com>
2025-07-17 13:11:06 -0600

    adding more messages

--- src/utils/messages/resultMessages.ts
+++ src/utils/messages/resultMessages.ts
@@ -1,10 +1,12 @@
 export const ResultMessages = {
   CREATED: { code: 201, message: "ENTITY_CREATED" },
   SUCCESS: { code: 200, message: "OPERATION_SUCCESS" },
-  ERROR: { code: 404, message: "ENTITY_NOT_FOUND" },
   BAD_REQUEST: { code: 400, message: "REQUIREMENTS_MISSING" },
   UNAUTHORIZED: { code: 401, message: "UNAUTHORIZED" },
+  BAD_CREDENTIALS: { code: 402, message: "BAD_CREDENTIALS" },
   FORBIDDEN: { code: 403, message: "FORBIDDEN" },
+  ERROR: { code: 404, message: "ENTITY_NOT_FOUND" },
+  ACCOUNT_DISABLED: { code: 405, message: "ACCOUNT_DISABLED" },
   INTERNAL_SERVER_ERROR: { code: 500, message: "UNKNOWN_SERVER_ERROR" },
   ENTITY_ALREADY_EXISTS: { code: 400, message: "ENTITY_ALREADY_EXISTS" },
   // ...add more as needed
