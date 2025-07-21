d2ebd49769b12ac56fe020837c403d6dae7356ba
Herbert Tamayo <hftamayo@gmail.com>
2025-07-21 11:43:44 -0600

    introducing ErrorResponseDTO class

---
+++ src/dto/ErrorResponse.dto.ts
@@ -0,0 +1,23 @@
+import { ErrorEvent } from "@/types/event.types";
+
+export class ErrorResponseDTO implements Pick<ErrorEvent, "code" | "resultMessage" | "debugMessage" | "timestamp"> {
+  code: number;
+  resultMessage: string;
+  debugMessage?: string;
+  timestamp: string;
+  cacheTTL: number;
+
+  constructor(options: {
+    code: number;
+    resultMessage: string;
+    debugMessage?: string;
+    timestamp?: number;
+    cacheTTL?: number;
+  }) {
+    this.code = options.code;
+    this.resultMessage = options.resultMessage;
+    this.debugMessage = options.debugMessage;
+    this.timestamp = options.timestamp ? new Date(options.timestamp).toISOString() : new Date().toISOString();
+    this.cacheTTL = options.cacheTTL ?? 0;
+  }
+} 
