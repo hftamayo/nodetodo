f4e3f1f90c95635f7d8b32d0fbc54a6c76d33fd3
Herbert Tamayo <hftamayo@gmail.com>
2025-07-21 11:08:05 -0600

    polishing crudOpsResponseDTO class

--- src/dto/crudOperationResponse.dto.ts
+++ src/dto/crudOperationResponse.dto.ts
@@ -9,15 +9,16 @@ export class CrudOperationResponseDto<T = any> {
   cacheTTL: number;
 
   constructor(options: {
-    resultType: ResultMessageKeys;
+    code: number;
+    resultMessage: string;
     data?: T;
     dataList?: T[];
     timestamp?: number;
     cacheTTL?: number;
   }) {
-    const { resultType, data, dataList, timestamp, cacheTTL } = options;
-    this.code = ResultMessages[resultType].code;
-    this.resultMessage = ResultMessages[resultType].message;
+    const { code, resultMessage, data, dataList, timestamp, cacheTTL } = options;
+    this.code = code;
+    this.resultMessage = resultMessage;
     this.data = data;
     this.dataList = dataList;
     this.timestamp = timestamp ?? Date.now();
