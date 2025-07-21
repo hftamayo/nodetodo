2afad0663af15abac15ae79c2594139ed668b847
Herbert Tamayo <hftamayo@gmail.com>
2025-07-21 12:07:47 -0600

    removing the use of any in rolesResponse

--- src/dto/roles/rolesResponse.dto.ts
+++ src/dto/roles/rolesResponse.dto.ts
@@ -2,12 +2,12 @@ import { FilteredRole } from "@/types/role.types";
 
 export class RolesResponseDTO {
   id: string;
-  [key: string]: any;
+  [key: string]: FilteredRole[keyof FilteredRole];
 
   constructor(role: FilteredRole) {
     this.id = (role as any)._id?.toString?.() ?? (role as any).id;
     Object.entries(role).forEach(([key, value]) => {
-      if (key !== "_id") this[key] = value;
+      if (key !== "_id" && key !== "id") this[key] = value;
     });
   }
 } 
